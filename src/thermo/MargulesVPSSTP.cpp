Cantera/cantera

[Input] Create Margules and PDSS_ConstVol objects from YAML definitions

src/thermo/MargulesVPSSTP.cpp
@@ -200,6 +200,26 @@ void MargulesVPSSTP::getPartialMolarVolumes(doublereal* vbar) const
 void MargulesVPSSTP::initThermo()
 {
     initLengths();
+    if (m_extra.hasKey("interactions")) {
+        for (auto& item : m_extra["interactions"].asVector<AnyMap>()) {
+            auto& species = item["species"].asVector<string>(2);
+            vector_fp h(2), s(2), vh(2), vs(2);
+            if (item.hasKey("excess-enthalpy")) {
+                h = item.convertVector("excess-enthalpy", "J/kmol", 2);
+            }
+            if (item.hasKey("excess-entropy")) {
+                s = item.convertVector("excess-entropy", "J/kmol/K", 2);
+            }
+            if (item.hasKey("excess-volume-enthalpy")) {
+                vh = item.convertVector("excess-volume-enthalpy", "m^3/kmol/K", 2);
+            }
+            if (item.hasKey("excess-volume-entropy")) {
+                vs = item.convertVector("excess-volume-entropy", "m^3/kmol/K", 2);
+            }
+            addBinaryInteraction(species[0], species[1],
+                h[0], h[1], s[0], s[1], vh[0], vh[1], vs[0], vs[1]);
+        }
+    }
     GibbsExcessVPSSTP::initThermo();
 }
 

src/thermo/PDSS_ConstVol.cpp
@@ -40,6 +40,9 @@ void PDSS_ConstVol::setParametersFromXML(const XML_Node& speciesNode)
 void PDSS_ConstVol::initThermo()
 {
     PDSS::initThermo();
+    if (m_extra.hasKey("molar-volume")) {
+        setMolarVolume(m_extra.convert("molar-volume", "m^3/kmol"));
+    }
     m_minTemp = m_spthermo->minTemp();
     m_maxTemp = m_spthermo->maxTemp();
     m_p0 = m_spthermo->refPressure();

JacobDannecker/cantera_ma

src/thermo/MargulesVPSSTP.cpp
@@ -200,6 +200,26 @@ void MargulesVPSSTP::getPartialMolarVolumes(doublereal* vbar) const
 void MargulesVPSSTP::initThermo()
 {
     initLengths();
+    if (m_input.hasKey("interactions")) {
+        for (auto& item : m_input["interactions"].asVector<AnyMap>()) {
+            auto& species = item["species"].asVector<string>(2);
+            vector_fp h(2), s(2), vh(2), vs(2);
+            if (item.hasKey("excess-enthalpy")) {
+                h = item.convertVector("excess-enthalpy", "J/kmol", 2);
+            }
+            if (item.hasKey("excess-entropy")) {
+                s = item.convertVector("excess-entropy", "J/kmol/K", 2);
+            }
+            if (item.hasKey("excess-volume-enthalpy")) {
+                vh = item.convertVector("excess-volume-enthalpy", "m^3/kmol/K", 2);
+            }
+            if (item.hasKey("excess-volume-entropy")) {
+                vs = item.convertVector("excess-volume-entropy", "m^3/kmol/K", 2);
+            }
+            addBinaryInteraction(species[0], species[1],
+                h[0], h[1], s[0], s[1], vh[0], vh[1], vs[0], vs[1]);
+        }
+    }
     GibbsExcessVPSSTP::initThermo();
 }
 

src/thermo/PDSS_ConstVol.cpp
@@ -40,6 +40,9 @@ void PDSS_ConstVol::setParametersFromXML(const XML_Node& speciesNode)
 void PDSS_ConstVol::initThermo()
 {
     PDSS::initThermo();
+    if (m_input.hasKey("molar-volume")) {
+        setMolarVolume(m_input.convert("molar-volume", "m^3/kmol"));
+    }
     m_minTemp = m_spthermo->minTemp();
     m_maxTemp = m_spthermo->maxTemp();
     m_p0 = m_spthermo->refPressure();