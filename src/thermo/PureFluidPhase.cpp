Cantera/cantera

[Input] Create PureFluid objects from YAML definitions

src/thermo/PureFluidPhase.cpp
@@ -31,6 +31,10 @@ PureFluidPhase::PureFluidPhase() :
 
 void PureFluidPhase::initThermo()
 {
+    if (m_extra.hasKey("pure-fluid-name")) {
+        setSubstance(m_extra["pure-fluid-name"].asString());
+    }
+
     if (m_tpx_name != "") {
         m_sub.reset(tpx::newSubstance(m_tpx_name));
     } else {

JacobDannecker/cantera_ma

src/thermo/PureFluidPhase.cpp
@@ -31,6 +31,10 @@ PureFluidPhase::PureFluidPhase() :
 
 void PureFluidPhase::initThermo()
 {
+    if (m_input.hasKey("pure-fluid-name")) {
+        setSubstance(m_input["pure-fluid-name"].asString());
+    }
+
     if (m_tpx_name != "") {
         m_sub.reset(tpx::newSubstance(m_tpx_name));
     } else {