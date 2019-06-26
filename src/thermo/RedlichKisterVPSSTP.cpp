Cantera/cantera

[Input] Create RedlichKister objects from YAML definitions

src/thermo/RedlichKisterVPSSTP.cpp
@@ -179,6 +179,16 @@ void RedlichKisterVPSSTP::getPartialMolarVolumes(doublereal* vbar) const
 
 void RedlichKisterVPSSTP::initThermo()
 {
+    if (m_extra.hasKey("interactions")) {
+        for (const auto& item : m_extra["interactions"].asVector<AnyMap>()) {
+            auto& species = item["species"].asVector<string>(2);
+            vector_fp h_excess = item.convertVector("excess-enthalpy", "J/kmol");
+            vector_fp s_excess = item.convertVector("excess-entropy", "J/kmol/K");
+            addBinaryInteraction(species[0], species[1],
+                                 h_excess.data(), h_excess.size(),
+                                 s_excess.data(), s_excess.size());
+        }
+    }
     initLengths();
     GibbsExcessVPSSTP::initThermo();
 }

JacobDannecker/cantera_ma

src/thermo/RedlichKisterVPSSTP.cpp
@@ -179,6 +179,16 @@ void RedlichKisterVPSSTP::getPartialMolarVolumes(doublereal* vbar) const
 
 void RedlichKisterVPSSTP::initThermo()
 {
+    if (m_input.hasKey("interactions")) {
+        for (const auto& item : m_input["interactions"].asVector<AnyMap>()) {
+            auto& species = item["species"].asVector<string>(2);
+            vector_fp h_excess = item.convertVector("excess-enthalpy", "J/kmol");
+            vector_fp s_excess = item.convertVector("excess-entropy", "J/kmol/K");
+            addBinaryInteraction(species[0], species[1],
+                                 h_excess.data(), h_excess.size(),
+                                 s_excess.data(), s_excess.size());
+        }
+    }
     initLengths();
     GibbsExcessVPSSTP::initThermo();
 }