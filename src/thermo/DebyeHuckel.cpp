Cantera/cantera

[Input] Create DebyeHuckel and PDSS_Water objects from YAML definitions

src/thermo/DebyeHuckel.cpp
@@ -307,15 +307,20 @@ static int interp_est(const std::string& estString)
 {
     if (caseInsensitiveEquals(estString, "solvent")) {
         return cEST_solvent;
-    } else if (caseInsensitiveEquals(estString, "chargedspecies")) {
+    } else if (estString == "charged-species"
+               || caseInsensitiveEquals(estString, "chargedspecies")) {
         return cEST_chargedSpecies;
-    } else if (caseInsensitiveEquals(estString, "weakacidassociated")) {
+    } else if (estString == "weak-acid-associated"
+               || caseInsensitiveEquals(estString, "weakacidassociated")) {
         return cEST_weakAcidAssociated;
-    } else if (caseInsensitiveEquals(estString, "strongacidassociated")) {
+    } else if (estString == "strong-acid-associated"
+               || caseInsensitiveEquals(estString, "strongacidassociated")) {
         return cEST_strongAcidAssociated;
-    } else if (caseInsensitiveEquals(estString, "polarneutral")) {
+    } else if (estString == "polar-neutral"
+               || caseInsensitiveEquals(estString, "polarneutral")) {
         return cEST_polarNeutral;
-    } else if (caseInsensitiveEquals(estString, "nonpolarneutral")) {
+    } else if (estString == "nonpolar-neutral"
+               || caseInsensitiveEquals(estString, "nonpolarneutral")) {
         return cEST_nonpolarNeutral;
     } else {
         throw CanteraError("interp_est (DebyeHuckel)",
@@ -324,16 +329,21 @@ static int interp_est(const std::string& estString)
 }
 
 void DebyeHuckel::setDebyeHuckelModel(const std::string& model) {
-    if (model == "" || caseInsensitiveEquals(model, "Dilute_limit")) {
+    if (model == ""
+        || model == "dilute-limit"
+        || caseInsensitiveEquals(model, "Dilute_limit")) {
         m_formDH = DHFORM_DILUTE_LIMIT;
-    } else if (caseInsensitiveEquals(model, "Bdot_with_variable_a")) {
+    } else if (model == "B-dot-with-variable-a"
+               || caseInsensitiveEquals(model, "Bdot_with_variable_a")) {
         m_formDH = DHFORM_BDOT_AK;
-    } else if (caseInsensitiveEquals(model, "Bdot_with_common_a")) {
+    } else if (model == "B-dot-with-common-a"
+               || caseInsensitiveEquals(model, "Bdot_with_common_a")) {
         m_formDH = DHFORM_BDOT_ACOMMON;
-    } else if (caseInsensitiveEquals(model, "Beta_ij")) {
+    } else if (caseInsensitiveEquals(model, "beta_ij")) {
         m_formDH = DHFORM_BETAIJ;
         m_Beta_ij.resize(m_kk, m_kk, 0.0);
-    } else if (caseInsensitiveEquals(model, "Pitzer_with_Beta_ij")) {
+    } else if (model == "Pitzer-with-beta_ij"
+               || caseInsensitiveEquals(model, "Pitzer_with_Beta_ij")) {
         m_formDH = DHFORM_PITZER_BETAIJ;
         m_Beta_ij.resize(m_kk, m_kk, 0.0);
     } else {
@@ -557,6 +567,35 @@ void DebyeHuckel::initThermoXML(XML_Node& phaseNode, const std::string& id_)
 void DebyeHuckel::initThermo()
 {
     MolalityVPSSTP::initThermo();
+    if (m_extra.hasKey("activity-data")) {
+        auto& node = m_extra["activity-data"].as<AnyMap>();
+        setDebyeHuckelModel(node["model"].asString());
+        if (node.hasKey("A_Debye")) {
+            setA_Debye(node.convert("A_Debye", "kg^0.5/gmol^0.5"));
+        }
+        if (node.hasKey("B_Debye")) {
+            setB_Debye(node.convert("B_Debye", "kg^0.5/gmol^0.5/m"));
+        }
+        if (node.hasKey("max-ionic-strength")) {
+            setMaxIonicStrength(node["max-ionic-strength"].asDouble());
+        }
+        if (node.hasKey("use-Helgeson-fixed-form")) {
+            useHelgesonFixedForm(node["use-Helgeson-fixed-form"].asBool());
+        }
+        if (node.hasKey("default-ionic-radius")) {
+            setDefaultIonicRadius(node.convert("default-ionic-radius", "m"));
+        }
+        if (node.hasKey("B-dot")) {
+            setB_dot(node["B-dot"].asDouble());
+        }
+        if (node.hasKey("beta")) {
+            for (auto& item : node["beta"].asVector<AnyMap>()) {
+                auto& species = item["species"].asVector<string>(2);
+                setBeta(species[0], species[1], item["beta"].asDouble());
+            }
+        }
+    }
+
     // Solvent
     m_waterSS = dynamic_cast<PDSS_Water*>(providePDSS(0));
     if (m_waterSS) {
@@ -697,11 +736,8 @@ bool DebyeHuckel::addSpecies(shared_ptr<Species> spec)
         m_B_Dot.push_back(0.0);
         m_tmpV.push_back(0.0);
 
-        if (spec->extra.hasKey("ionic_radius")) {
-            m_Aionic.push_back(spec->extra["ionic_radius"].asDouble());
-        } else {
-            m_Aionic.push_back(NAN);  // NAN will be replaced with default value
-        }
+        // NAN will be replaced with default value
+        m_Aionic.push_back(spec->extra.convert("ionic-radius", "m", NAN));
 
         // Guess electrolyte species type based on charge properties
         int est = cEST_nonpolarNeutral;
@@ -709,8 +745,8 @@ bool DebyeHuckel::addSpecies(shared_ptr<Species> spec)
         if (fabs(spec->charge) > 0.0001) {
             est = cEST_chargedSpecies;
         }
-        if (spec->extra.hasKey("weak_acid_charge")) {
-            stoichCharge = spec->extra["weak_acid_charge"].asDouble();
+        if (spec->extra.hasKey("weak-acid-charge")) {
+            stoichCharge = spec->extra["weak-acid-charge"].asDouble();
             if (fabs(stoichCharge - spec->charge) > 0.0001) {
                 est = cEST_weakAcidAssociated;
             }
@@ -722,8 +758,8 @@ bool DebyeHuckel::addSpecies(shared_ptr<Species> spec)
         }
 
         // Apply override of the electrolyte species type
-        if (spec->extra.hasKey("electrolyte_species_type")) {
-            est = interp_est(spec->extra["electrolyte_species_type"].asString());
+        if (spec->extra.hasKey("electrolyte-species-type")) {
+            est = interp_est(spec->extra["electrolyte-species-type"].asString());
         }
         m_electrolyteSpeciesType.push_back(est);
     }

src/thermo/Species.cpp
@@ -58,11 +58,11 @@ shared_ptr<Species> newSpecies(const XML_Node& species_node)
 
     // Extra data used for some electrolyte species
     if (species_node.hasChild("stoichIsMods")) {
-        s->extra["weak_acid_charge"] = getFloat(species_node, "stoichIsMods");
+        s->extra["weak-acid-charge"] = getFloat(species_node, "stoichIsMods");
     }
 
     if (species_node.hasChild("electrolyteSpeciesType")) {
-        s->extra["electrolyte_species_type"] = species_node.child("electrolyteSpeciesType").value();
+        s->extra["electrolyte-species-type"] = species_node.child("electrolyteSpeciesType").value();
     }
 
     // Extra data optionally used by LatticePhase

test/thermo/phaseConstructors.cpp
@@ -426,22 +426,22 @@ TEST(DebyeHuckel, fromScratch)
     auto sNa = make_species("Na+", "Na:1, E:-1", -240.34e6,
                               298.15, -103.98186, 333.15, -103.98186);
     sNa->charge = 1;
-    sNa->extra["ionic_radius"] = 4.0e-10;
+    sNa->extra["ionic-radius"] = 4.0e-10;
     auto sCl = make_species("Cl-", "Cl:1, E:1", -167.08e6,
                               298.15, -74.20664, 333.15, -74.20664);
     sCl->charge = -1;
-    sCl->extra["ionic_radius"] = 3.0e-10;
+    sCl->extra["ionic-radius"] = 3.0e-10;
     auto sH = make_species("H+", "H:1, E:-1", 0.0, 298.15, 0.0, 333.15, 0.0);
     sH->charge = 1;
-    sH->extra["ionic_radius"] = 9.0e-10;
+    sH->extra["ionic-radius"] = 9.0e-10;
     auto sOH = make_species("OH-", "O:1, H:1, E:1", -230.015e6,
                               298.15, -91.50963, 333.15, -85);
     sOH->charge = -1;
-    sOH->extra["ionic_radius"] = 3.5e-10;
+    sOH->extra["ionic-radius"] = 3.5e-10;
     auto sNaCl = make_species("NaCl(aq)", "Na:1, Cl:1", -96.03e6*4.184,
                               298.15, -174.5057463, 333.15, -174.5057463);
-    sNaCl->extra["weak_acid_charge"] = -1.0;
-    sNaCl->extra["electrolyte_species_type"] = "weakAcidAssociated";
+    sNaCl->extra["weak-acid-charge"] = -1.0;
+    sNaCl->extra["electrolyte-species-type"] = "weakAcidAssociated";
     for (auto& s : {sH2O, sNa, sCl, sH, sOH, sNaCl}) {
         p.addSpecies(s);
     }

JacobDannecker/cantera_ma

src/thermo/DebyeHuckel.cpp
@@ -307,15 +307,20 @@ static int interp_est(const std::string& estString)
 {
     if (caseInsensitiveEquals(estString, "solvent")) {
         return cEST_solvent;
-    } else if (caseInsensitiveEquals(estString, "chargedspecies")) {
+    } else if (estString == "charged-species"
+               || caseInsensitiveEquals(estString, "chargedspecies")) {
         return cEST_chargedSpecies;
-    } else if (caseInsensitiveEquals(estString, "weakacidassociated")) {
+    } else if (estString == "weak-acid-associated"
+               || caseInsensitiveEquals(estString, "weakacidassociated")) {
         return cEST_weakAcidAssociated;
-    } else if (caseInsensitiveEquals(estString, "strongacidassociated")) {
+    } else if (estString == "strong-acid-associated"
+               || caseInsensitiveEquals(estString, "strongacidassociated")) {
         return cEST_strongAcidAssociated;
-    } else if (caseInsensitiveEquals(estString, "polarneutral")) {
+    } else if (estString == "polar-neutral"
+               || caseInsensitiveEquals(estString, "polarneutral")) {
         return cEST_polarNeutral;
-    } else if (caseInsensitiveEquals(estString, "nonpolarneutral")) {
+    } else if (estString == "nonpolar-neutral"
+               || caseInsensitiveEquals(estString, "nonpolarneutral")) {
         return cEST_nonpolarNeutral;
     } else {
         throw CanteraError("interp_est (DebyeHuckel)",
@@ -324,16 +329,21 @@ static int interp_est(const std::string& estString)
 }
 
 void DebyeHuckel::setDebyeHuckelModel(const std::string& model) {
-    if (model == "" || caseInsensitiveEquals(model, "Dilute_limit")) {
+    if (model == ""
+        || model == "dilute-limit"
+        || caseInsensitiveEquals(model, "Dilute_limit")) {
         m_formDH = DHFORM_DILUTE_LIMIT;
-    } else if (caseInsensitiveEquals(model, "Bdot_with_variable_a")) {
+    } else if (model == "B-dot-with-variable-a"
+               || caseInsensitiveEquals(model, "Bdot_with_variable_a")) {
         m_formDH = DHFORM_BDOT_AK;
-    } else if (caseInsensitiveEquals(model, "Bdot_with_common_a")) {
+    } else if (model == "B-dot-with-common-a"
+               || caseInsensitiveEquals(model, "Bdot_with_common_a")) {
         m_formDH = DHFORM_BDOT_ACOMMON;
-    } else if (caseInsensitiveEquals(model, "Beta_ij")) {
+    } else if (caseInsensitiveEquals(model, "beta_ij")) {
         m_formDH = DHFORM_BETAIJ;
         m_Beta_ij.resize(m_kk, m_kk, 0.0);
-    } else if (caseInsensitiveEquals(model, "Pitzer_with_Beta_ij")) {
+    } else if (model == "Pitzer-with-beta_ij"
+               || caseInsensitiveEquals(model, "Pitzer_with_Beta_ij")) {
         m_formDH = DHFORM_PITZER_BETAIJ;
         m_Beta_ij.resize(m_kk, m_kk, 0.0);
     } else {
@@ -557,6 +567,40 @@ void DebyeHuckel::initThermoXML(XML_Node& phaseNode, const std::string& id_)
 void DebyeHuckel::initThermo()
 {
     MolalityVPSSTP::initThermo();
+    if (m_input.hasKey("activity-data")) {
+        auto& node = m_input["activity-data"].as<AnyMap>();
+        setDebyeHuckelModel(node["model"].asString());
+        if (node.hasKey("A_Debye")) {
+            if (node["A_Debye"].is<string>()
+                && node["A_Debye"].asString() == "variable") {
+                setA_Debye(-1);
+            } else {
+                setA_Debye(node.convert("A_Debye", "kg^0.5/gmol^0.5"));
+            }
+        }
+        if (node.hasKey("B_Debye")) {
+            setB_Debye(node.convert("B_Debye", "kg^0.5/gmol^0.5/m"));
+        }
+        if (node.hasKey("max-ionic-strength")) {
+            setMaxIonicStrength(node["max-ionic-strength"].asDouble());
+        }
+        if (node.hasKey("use-Helgeson-fixed-form")) {
+            useHelgesonFixedForm(node["use-Helgeson-fixed-form"].asBool());
+        }
+        if (node.hasKey("default-ionic-radius")) {
+            setDefaultIonicRadius(node.convert("default-ionic-radius", "m"));
+        }
+        if (node.hasKey("B-dot")) {
+            setB_dot(node["B-dot"].asDouble());
+        }
+        if (node.hasKey("beta")) {
+            for (auto& item : node["beta"].asVector<AnyMap>()) {
+                auto& species = item["species"].asVector<string>(2);
+                setBeta(species[0], species[1], item["beta"].asDouble());
+            }
+        }
+    }
+
     // Solvent
     m_waterSS = dynamic_cast<PDSS_Water*>(providePDSS(0));
     if (m_waterSS) {
@@ -697,11 +741,8 @@ bool DebyeHuckel::addSpecies(shared_ptr<Species> spec)
         m_B_Dot.push_back(0.0);
         m_tmpV.push_back(0.0);
 
-        if (spec->extra.hasKey("ionic_radius")) {
-            m_Aionic.push_back(spec->extra["ionic_radius"].asDouble());
-        } else {
-            m_Aionic.push_back(NAN);  // NAN will be replaced with default value
-        }
+        // NAN will be replaced with default value
+        m_Aionic.push_back(spec->input.convert("ionic-radius", "m", NAN));
 
         // Guess electrolyte species type based on charge properties
         int est = cEST_nonpolarNeutral;
@@ -709,8 +750,8 @@ bool DebyeHuckel::addSpecies(shared_ptr<Species> spec)
         if (fabs(spec->charge) > 0.0001) {
             est = cEST_chargedSpecies;
         }
-        if (spec->extra.hasKey("weak_acid_charge")) {
-            stoichCharge = spec->extra["weak_acid_charge"].asDouble();
+        if (spec->input.hasKey("weak-acid-charge")) {
+            stoichCharge = spec->input["weak-acid-charge"].asDouble();
             if (fabs(stoichCharge - spec->charge) > 0.0001) {
                 est = cEST_weakAcidAssociated;
             }
@@ -722,8 +763,8 @@ bool DebyeHuckel::addSpecies(shared_ptr<Species> spec)
         }
 
         // Apply override of the electrolyte species type
-        if (spec->extra.hasKey("electrolyte_species_type")) {
-            est = interp_est(spec->extra["electrolyte_species_type"].asString());
+        if (spec->input.hasKey("electrolyte-species-type")) {
+            est = interp_est(spec->input["electrolyte-species-type"].asString());
         }
         m_electrolyteSpeciesType.push_back(est);
     }

src/thermo/Species.cpp
@@ -58,11 +58,11 @@ shared_ptr<Species> newSpecies(const XML_Node& species_node)
 
     // Extra data used for some electrolyte species
     if (species_node.hasChild("stoichIsMods")) {
-        s->extra["weak_acid_charge"] = getFloat(species_node, "stoichIsMods");
+        s->input["weak-acid-charge"] = getFloat(species_node, "stoichIsMods");
     }
 
     if (species_node.hasChild("electrolyteSpeciesType")) {
-        s->extra["electrolyte_species_type"] = species_node.child("electrolyteSpeciesType").value();
+        s->input["electrolyte-species-type"] = species_node.child("electrolyteSpeciesType").value();
     }
 
     // Extra data optionally used by LatticePhase

test/thermo/phaseConstructors.cpp
@@ -426,22 +426,22 @@ TEST(DebyeHuckel, fromScratch)
     auto sNa = make_species("Na+", "Na:1, E:-1", -240.34e6,
                               298.15, -103.98186, 333.15, -103.98186);
     sNa->charge = 1;
-    sNa->extra["ionic_radius"] = 4.0e-10;
+    sNa->input["ionic-radius"] = 4.0e-10;
     auto sCl = make_species("Cl-", "Cl:1, E:1", -167.08e6,
                               298.15, -74.20664, 333.15, -74.20664);
     sCl->charge = -1;
-    sCl->extra["ionic_radius"] = 3.0e-10;
+    sCl->input["ionic-radius"] = 3.0e-10;
     auto sH = make_species("H+", "H:1, E:-1", 0.0, 298.15, 0.0, 333.15, 0.0);
     sH->charge = 1;
-    sH->extra["ionic_radius"] = 9.0e-10;
+    sH->input["ionic-radius"] = 9.0e-10;
     auto sOH = make_species("OH-", "O:1, H:1, E:1", -230.015e6,
                               298.15, -91.50963, 333.15, -85);
     sOH->charge = -1;
-    sOH->extra["ionic_radius"] = 3.5e-10;
+    sOH->input["ionic-radius"] = 3.5e-10;
     auto sNaCl = make_species("NaCl(aq)", "Na:1, Cl:1", -96.03e6*4.184,
                               298.15, -174.5057463, 333.15, -174.5057463);
-    sNaCl->extra["weak_acid_charge"] = -1.0;
-    sNaCl->extra["electrolyte_species_type"] = "weakAcidAssociated";
+    sNaCl->input["weak-acid-charge"] = -1.0;
+    sNaCl->input["electrolyte-species-type"] = "weakAcidAssociated";
     for (auto& s : {sH2O, sNa, sCl, sH, sOH, sNaCl}) {
         p.addSpecies(s);
     }