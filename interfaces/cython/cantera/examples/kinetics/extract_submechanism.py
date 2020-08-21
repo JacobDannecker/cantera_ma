JacobDannecker/cantera_ma

commit eb35aa4e07fbe3875238108e516b26cb75af9154

[YAML] Switch interfaces and examples to use YAML files

interfaces/cython/cantera/examples/kinetics/extract_submechanism.py
@@ -13,7 +13,7 @@ import cantera as ct
 import numpy as np
 import matplotlib.pyplot as plt
 
-all_species = ct.Species.listFromFile('gri30.xml')
+all_species = ct.Species.listFromFile('gri30.yaml')
 species = []
 
 # Filter species
@@ -35,7 +35,7 @@ species_names = {S.name for S in species}
 print('Species: {0}'.format(', '.join(S.name for S in species)))
 
 # Filter reactions, keeping only those that only involve the selected species
-all_reactions = ct.Reaction.listFromFile('gri30.xml')
+all_reactions = ct.Reaction.listFromFile('gri30.yaml')
 reactions = []
 
 print('\nReactions:')
@@ -50,7 +50,7 @@ for R in all_reactions:
     print(R.equation)
 print('\n')
 
-gas1 = ct.Solution('gri30.xml')
+gas1 = ct.Solution('gri30.yaml')
 gas2 = ct.Solution(thermo='IdealGas', kinetics='GasKinetics',
                    species=species, reactions=reactions)
 

commit b211080a5655a12261aca606fd9fd29d7f14d62f

[Cython/Examples] Fix extract_submechanism example
When loading reactions from a YAML file, an instance of a Kinetics
object is required so that the species associated with the reactions are
available. The required object is now constructed from the already
loaded species definitions.

Fixes #821, problem introduced in eb35aa4e07fbe3875238108e516b26cb75af9154

interfaces/cython/cantera/examples/kinetics/extract_submechanism.py
@@ -14,7 +14,8 @@ from timeit import default_timer
 import cantera as ct
 import matplotlib.pyplot as plt
 
-all_species = ct.Species.listFromFile('gri30.yaml')
+input_file = 'gri30.yaml'
+all_species = ct.Species.listFromFile(input_file)
 species = []
 
 # Filter species
@@ -36,7 +37,8 @@ species_names = {S.name for S in species}
 print('Species: {0}'.format(', '.join(S.name for S in species)))
 
 # Filter reactions, keeping only those that only involve the selected species
-all_reactions = ct.Reaction.listFromFile('gri30.yaml')
+ref_phase = ct.Solution(thermo='ideal-gas', kinetics='gas', species=all_species)
+all_reactions = ct.Reaction.listFromFile(input_file, ref_phase)
 reactions = []
 
 print('\nReactions:')
@@ -51,8 +53,8 @@ for R in all_reactions:
     print(R.equation)
 print('\n')
 
-gas1 = ct.Solution('gri30.yaml')
-gas2 = ct.Solution(thermo='IdealGas', kinetics='GasKinetics',
+gas1 = ct.Solution(input_file)
+gas2 = ct.Solution(thermo='ideal-gas', kinetics='gas',
                    species=species, reactions=reactions)
 
 