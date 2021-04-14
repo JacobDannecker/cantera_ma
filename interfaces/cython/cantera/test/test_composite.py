JacobDannecker/cantera_ma

commit 5b5eb15444908affd0155c1e52e1fee89ede1922

[Input] Move shared info from AnyMap and AnyValue to a common base class

src/base/AnyMap.cpp
@@ -265,12 +265,32 @@ std::map<std::string, std::string> AnyValue::s_typenames = {
 
 std::unordered_map<std::string, std::pair<AnyMap, int>> AnyMap::s_cache;
 
+// Methods of class AnyBase
+
+AnyBase::AnyBase()
+    : m_line(-1)
+    , m_column(-1)
+{}
+
+void AnyBase::setLoc(int line, int column)
+{
+    m_line = line;
+    m_column = column;
+}
+
+const AnyValue& AnyBase::getMetadata(const std::string& key) const
+{
+    if (m_metadata && m_metadata->hasKey(key)) {
+        return m_metadata->at(key);
+    } else {
+        return Empty;
+    }
+}
+
 // Methods of class AnyValue
 
 AnyValue::AnyValue()
-  : m_line(-1)
-  , m_column(-1)
-  , m_key()
+  : m_key()
   , m_value(new boost::any{})
   , m_equals(eq_comparer<size_t>)
 {}
@@ -278,9 +298,7 @@ AnyValue::AnyValue()
 AnyValue::~AnyValue() = default;
 
 AnyValue::AnyValue(AnyValue const& other)
-    : m_line(other.m_line)
-    , m_column(other.m_column)
-    , m_metadata(other.m_metadata)
+    : AnyBase(other)
     , m_key(other.m_key)
     , m_value(new boost::any{*other.m_value})
     , m_equals(other.m_equals)
@@ -288,9 +306,7 @@ AnyValue::AnyValue(AnyValue const& other)
 }
 
 AnyValue::AnyValue(AnyValue&& other)
-    : m_line(other.m_line)
-    , m_column(other.m_column)
-    , m_metadata(std::move(other.m_metadata))
+    : AnyBase(std::move(other))
     , m_key(std::move(other.m_key))
     , m_value(std::move(other.m_value))
     , m_equals(std::move(other.m_equals))
@@ -298,11 +314,10 @@ AnyValue::AnyValue(AnyValue&& other)
 }
 
 AnyValue& AnyValue::operator=(AnyValue const& other) {
-    if (this == &other)
+    if (this == &other) {
         return *this;
-    m_line = other.m_line;
-    m_column = other.m_column;
-    m_metadata = other.m_metadata;
+    }
+    AnyBase::operator=(*this);
     m_key = other.m_key;
     m_value.reset(new boost::any{*other.m_value});
     m_equals = other.m_equals;
@@ -310,11 +325,10 @@ AnyValue& AnyValue::operator=(AnyValue const& other) {
 }
 
 AnyValue& AnyValue::operator=(AnyValue&& other) {
-    if (this == &other)
+    if (this == &other) {
         return *this;
-    m_line = other.m_line;
-    m_column = other.m_column;
-    m_metadata = std::move(other.m_metadata);
+    }
+    AnyBase::operator=(std::move(other));
     m_key = std::move(other.m_key);
     m_value = std::move(other.m_value);
     m_equals = std::move(other.m_equals);
@@ -349,21 +363,6 @@ void AnyValue::setKey(const std::string &key) { m_key = key; }
 
 const std::type_info &AnyValue::type() const {
     return m_value->type();
-}
-
-void AnyValue::setLoc(int line, int column)
-{
-    m_line = line;
-    m_column = column;
-}
-
-const AnyValue& AnyValue::getMetadata(const std::string& key) const
-{
-    if (m_metadata && m_metadata->hasKey(key)) {
-        return m_metadata->at(key);
-    } else {
-        return Empty;
-    }
 }
 
 void AnyValue::propagateMetadata(shared_ptr<AnyMap>& metadata)
@@ -996,21 +995,6 @@ std::string AnyMap::keys_str() const
         ++iter;
     }
     return to_string(b);
-}
-
-void AnyMap::setLoc(int line, int column)
-{
-    m_line = line;
-    m_column = column;
-}
-
-const AnyValue& AnyMap::getMetadata(const std::string& key) const
-{
-    if (m_metadata && m_metadata->hasKey(key)) {
-        return m_metadata->at(key);
-    } else {
-        return Empty;
-    }
 }
 
 void AnyMap::propagateMetadata(shared_ptr<AnyMap>& metadata)

include/cantera/base/AnyMap.h
@@ -25,6 +25,36 @@ class any;
 namespace Cantera
 {
 
+//! Base class defining common data possessed by both AnyMap and AnyValue
+//! objects.
+class AnyBase {
+public:
+    AnyBase();
+    virtual ~AnyBase() {};
+
+    //! For values which are derived from an input file, set the line and column
+    //! of this value in that file. Used for providing context for some error
+    //! messages.
+    void setLoc(int line, int column);
+
+    //! Get a value from the metadata applicable to the AnyMap tree containing
+    //! this node.
+    const AnyValue& getMetadata(const std::string& key) const;
+
+protected:
+    //! Line where this node occurs in the input file
+    int m_line;
+
+    //! Column where this node occurs in the input file
+    int m_column;
+
+    //! Metadata relevant to an entire AnyMap tree, such as information about
+    // the input file used to create it
+    shared_ptr<AnyMap> m_metadata;
+
+    friend class InputFileError;
+};
+
 class AnyMap;
 
 //! A wrapper for a variable whose type is determined at runtime
@@ -43,7 +73,7 @@ class AnyMap;
  * - `std::string`
  * - `std::vector` of any of the above
  */
-class AnyValue
+class AnyValue : public AnyBase
 {
 public:
     AnyValue();
@@ -67,15 +97,6 @@ public:
     //! Set the name of the key storing this value in an AnyMap. Used for
     //! providing informative error messages in class InputFileError.
     void setKey(const std::string& key);
-
-    //! For values which are derived from an input file, set the line and column
-    //! of this value in that file. Used for providing context for some error
-    //! messages.
-    void setLoc(int line, int column);
-
-    //! Get a value from the metadata applicable to the AnyMap tree containing
-    //! this AnyValue.
-    const AnyValue& getMetadata(const std::string& key) const;
 
     //! Propagate metadata to any child elements
     void propagateMetadata(shared_ptr<AnyMap>& file);
@@ -209,16 +230,6 @@ private:
     template<class T>
     void checkSize(const std::vector<T>& v, size_t nMin, size_t nMax) const;
 
-    //! Line where this value occurs in the input file
-    int m_line;
-
-    //! Column where this value occurs in the input file
-    int m_column;
-
-    //! Metadata relevant to an entire AnyMap tree, such as information about
-    // the input file used to create it
-    shared_ptr<AnyMap> m_metadata;
-
     //! Key of this value in a parent `AnyMap`
     std::string m_key;
 
@@ -247,8 +258,6 @@ private:
     static bool vector2_eq(const boost::any& lhs, const boost::any& rhs);
 
     mutable Comparer m_equals;
-
-    friend class InputFileError;
 };
 
 //! Implicit conversion to vector<AnyValue>
@@ -347,7 +356,7 @@ std::vector<AnyMap>& AnyValue::asVector<AnyMap>(size_t nMin, size_t nMax);
  * }
  * ```
  */
-class AnyMap
+class AnyMap : public AnyBase
 {
 public:
     AnyMap(): m_units() {};
@@ -385,18 +394,9 @@ public:
     //! messages
     std::string keys_str() const;
 
-    //! For AnyMaps which are derived from an input file, set the line and
-    //! column of this AnyMap in that file. Used for providing context for some
-    //! error messages.
-    void setLoc(int line, int column);
-
     //! Set a metadata value that applies to this AnyMap and its children.
     //! Mainly for internal use in reading or writing from files.
     void setMetadata(const std::string& key, const AnyValue& value);
-
-    //! Get a value from the metadata applicable to the AnyMap tree containing
-    //! this AnyMap.
-    const AnyValue& getMetadata(const std::string& key) const;
 
     //! Propagate metadata to any child elements
     void propagateMetadata(shared_ptr<AnyMap>& file);
@@ -516,23 +516,12 @@ private:
     //! The default units that are used to convert stored values
     UnitSystem m_units;
 
-    //! Starting line for this map in the input file
-    int m_line;
-
-    //! Starting column for this map in the input file
-    int m_column;
-
-    //! Metadata relevant to an entire AnyMap tree, such as information about
-    // the input file used to create it
-    shared_ptr<AnyMap> m_metadata;
-
     //! Cache for previously-parsed input (YAML) files. The key is the full path
     //! to the file, and the second element of the value is the last-modified
     //! time for the file, which is used to enable change detection.
     static std::unordered_map<std::string, std::pair<AnyMap, int>> s_cache;
 
     friend class AnyValue;
-    friend class InputFileError;
 };
 
 // Define begin() and end() to allow use with range-based for loops
@@ -553,20 +542,7 @@ public:
     //! `node`. The `message` and `args` are processed as in the CanteraError
     //! class.
     template <typename... Args>
-    InputFileError(const std::string& procedure, const AnyValue& node,
-                   const std::string& message, const Args&... args)
-        : CanteraError(
-            procedure,
-            formatError(fmt::format(message, args...),
-                        node.m_line, node.m_column, node.m_metadata))
-        {
-        }
-
-    //! Indicate an error occurring in `procedure` while using information from
-    //! `node`. The `message` and `args` are processed as in the CanteraError
-    //! class.
-    template <typename... Args>
-    InputFileError(const std::string& procedure, const AnyMap& node,
+    InputFileError(const std::string& procedure, const AnyBase& node,
                    const std::string& message, const Args&... args)
         : CanteraError(
             procedure,

commit 12163bfd327151e56c29b4a1b9c85f0922be2561

[Input] Fix key ordering when outputting to Python
Fixes an error in the AnyValue assignment operator introduced in 5b5eb1544.

src/base/AnyMap.cpp
@@ -614,7 +614,7 @@ AnyValue& AnyValue::operator=(AnyValue const& other) {
     if (this == &other) {
         return *this;
     }
-    AnyBase::operator=(*this);
+    AnyBase::operator=(other);
     m_key = other.m_key;
     m_value.reset(new boost::any{*other.m_value});
     m_equals = other.m_equals;

interfaces/cython/cantera/test/test_composite.py
@@ -1,5 +1,6 @@
 from os.path import join as pjoin
 import os
+import sys
 
 import numpy as np
 from collections import OrderedDict
@@ -449,6 +450,17 @@ class TestSolutionSerialization(utilities.CanteraTest):
         data = gas.input_data
         self.assertEqual(data['custom-field']['first'], True)
         self.assertEqual(data['custom-field']['last'], [100, 200, 300])
+
+        if sys.version_info >= (3,7):
+            # Check that items are ordered as expected
+            self.assertEqual(
+                list(data),
+                ['name', 'thermo', 'elements', 'species', 'state', 'custom-field']
+            )
+            self.assertEqual(
+                list(data['custom-field']),
+                ['first', 'second', 'last']
+            )
 
     def test_input_data_debye_huckel(self):
         soln = ct.Solution('thermo-models.yaml', 'debye-huckel-B-dot-ak')