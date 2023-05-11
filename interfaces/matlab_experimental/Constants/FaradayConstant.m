ssun30/cantera

Changed name of constants to be consistent with C++ core.

interfaces/matlab_experimental/Constants/FaradayConstant.m
@@ -1,7 +1,7 @@
-function r = faradayconstant
+function r = Faraday
     % Get the Faraday constant in C/kmol of electron. ::
     %
-    %     >> r = faradayconstant
+    %     >> r = FaradayConstant
     %
     % :return:
     %     The Faraday constant in C/kmol-e.

JacobDannecker/cantera_ma

interfaces/matlab_experimental/Constants/FaradayConstant.m
@@ -1,7 +1,7 @@
-function r = faradayconstant
+function r = FaradayConstant
     % Get the Faraday constant in C/kmol of electron. ::
     %
-    %     >> r = faradayconstant
+    %     >> r = FaradayConstant
     %
     % :return:
     %     The Faraday constant in C/kmol-e.