ssun30/cantera

Changed all callct2 to ctString

interfaces/matlab_experimental/Utility/ctIsLoaded.m
@@ -1,0 +1,8 @@
+function i = ctIsLoaded()
+    if ~libisloaded(ctSharedLibrary)
+        error('Cantera is not loaded');
+    end
+
+    i = true;
+
+end

interfaces/matlab_experimental/Utility/ctString.m
@@ -1,24 +1,24 @@
-function output = callct2(varargin)
+function output = ctString(varargin)
     % Calls Cantera library functions with string outputs and returns
     % errors if necessary.
 
     err1 = -1;
 
     funcName = varargin{1};
-    buflen = calllib(ct, funcName, varargin{2:end}, 0, '');
+    buflen = calllib(ctSharedLibrary, funcName, varargin{2:end}, 0, '');
 
     if buflen > 0
         aa = char(ones(1, buflen));
         ptr = libpointer('cstring', aa);
-        [iok, bb] = calllib(ct, funcName, varargin{2:end}, buflen, ptr);
+        [iok, bb] = calllib(ctSharedLibrary, funcName, varargin{2:end}, buflen, ptr);
         output = bb;
         clear aa bb ptr;
     else
-        error(geterr);
+        error(ctGetErr);
     end
 
     if iok == -err1
-        error(geterr);
+        error(ctGetErr);
     end
 
 end

JacobDannecker/cantera_ma

interfaces/matlab_experimental/Utility/ctIsLoaded.m
@@ -1,0 +1,8 @@
+function i = ctIsLoaded()
+    if ~libisloaded(ctLib)
+        error('Cantera is not loaded');
+    end
+
+    i = true;
+
+end

interfaces/matlab_experimental/Utility/ctString.m
@@ -1,24 +1,24 @@
-function output = callct2(varargin)
+function output = ctString(varargin)
     % Calls Cantera library functions with string outputs and returns
     % errors if necessary.
 
     err1 = -1;
 
     funcName = varargin{1};
-    buflen = calllib(ct, funcName, varargin{2:end}, 0, '');
+    buflen = calllib(ctLib, funcName, varargin{2:end}, 0, '');
 
     if buflen > 0
         aa = char(ones(1, buflen));
         ptr = libpointer('cstring', aa);
-        [iok, bb] = calllib(ct, funcName, varargin{2:end}, buflen, ptr);
+        [iok, bb] = calllib(ctLib, funcName, varargin{2:end}, buflen, ptr);
         output = bb;
         clear aa bb ptr;
     else
-        error(geterr);
+        error(ctGetErr);
     end
 
     if iok == -err1
-        error(geterr);
+        error(ctGetErr);
     end
 
 end