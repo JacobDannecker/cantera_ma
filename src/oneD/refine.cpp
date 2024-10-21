Cantera/cantera

[oneD] changed names to be clearer in the refiner

src/oneD/refine.cpp
@@ -50,24 +50,27 @@ int Refiner::analyze(size_t n, const double* z, const double* x)
         return 0;
     }
 
-    m_loc.clear();
-    m_c.clear();
+    // check consistency
+    if (n != m_domain->nPoints()) {
+        throw CanteraError("Refiner::analyze", "number of grid points provided does not match domain size.");
+    }
+
+    // Reset the state of the refiner
+    m_insertion_points.clear();
+    m_component_name.clear();
     m_keep.clear();
 
+    // Keep the first and last grid points
     m_keep[0] = 1;
     m_keep[n-1] = 1;
 
     m_nv = m_domain->nComponents();
 
-    // check consistency
-    if (n != m_domain->nPoints()) {
-        throw CanteraError("Refiner::analyze", "inconsistent");
-    }
-
     // find locations where cell size ratio is too large.
-    vector<double> v(n), s(n-1);
+    vector<double> val(n);
+    vector<double> slope(n-1);
 
-    vector<double> dz(n-1);
+    vector<double> dz(n-1); // Store the right-looking grid spacings
     for (size_t j = 0; j < n-1; j++) {
         dz[j] = z[j+1] - z[j];
     }
@@ -77,39 +80,38 @@ int Refiner::analyze(size_t n, const double* z, const double* x)
             string name = m_domain->componentName(i);
             // get component i at all points
             for (size_t j = 0; j < n; j++) {
-                v[j] = value(x, i, j);
+                val[j] = value(x, i, j);
             }
 
             // slope of component i
             for (size_t j = 0; j < n-1; j++) {
-                s[j] = (value(x, i, j+1) - value(x, i, j))/(z[j+1] - z[j]);
+                slope[j] = (val[j+1] - val[j]) / dz[j];
             }
 
-            // find the range of values and slopes
-            double vmin = *min_element(v.begin(), v.end());
-            double vmax = *max_element(v.begin(), v.end());
-            double smin = *min_element(s.begin(), s.end());
-            double smax = *max_element(s.begin(), s.end());
+            // find the range of values and slopes of component i over the domain
+            double val_min = *min_element(val.begin(), val.end());
+            double val_max = *max_element(val.begin(), val.end());
+            double slope_min = *min_element(slope.begin(), slope.end());
+            double slope_max = *max_element(slope.begin(), slope.end());
 
-            // max absolute values of v and s
-            double aa = std::max(fabs(vmax), fabs(vmin));
-            double ss = std::max(fabs(smax), fabs(smin));
+            // max absolute values of val and slope
+            double val_amplitude = std::max(fabs(val_max), fabs(val_min));
+            double slope_amplitude = std::max(fabs(slope_max), fabs(slope_min));
 
-            // refine based on component i only if the range of v is
-            // greater than a fraction 'min_range' of max |v|. This
-            // eliminates components that consist of small fluctuations
-            // on a constant background.
-            if ((vmax - vmin) > m_min_range*aa) {
-                // maximum allowable difference in value between adjacent
-                // points.
-                double dmax = m_slope*(vmax - vmin) + m_thresh;
+            // refine based on component i only if the range of val is greater than a
+            // fraction 'min_range' of max |val|. This eliminates components that
+            // consist of small fluctuations around a constant value.
+            if ((val_max - val_min) > m_min_range*val_amplitude) {
+                // maximum allowable difference in value between adjacent points. Based
+                // on the global min and max values of the component over the domain.
+                double max_change = m_slope*(val_max - val_min) + m_thresh;
                 for (size_t j = 0; j < n-1; j++) {
-                    double r = fabs(v[j+1] - v[j])/dmax;
-                    if (r > 1.0 && dz[j] >= 2 * m_gridmin) {
-                        m_loc.insert(j);
-                        m_c.insert(name);
+                    double ratio = fabs(val[j+1] - val[j]) / max_change;
+                    if (ratio > 1.0 && dz[j] >= 2 * m_gridmin) {
+                        m_insertion_points.insert(j);
+                        m_component_name.insert(name);
                     }
-                    if (r >= m_prune) {
+                    if (ratio >= m_prune) {
                         m_keep[j] = 1;
                         m_keep[j+1] = 1;
                     } else if (m_keep[j] == 0) {
@@ -118,23 +120,21 @@ int Refiner::analyze(size_t n, const double* z, const double* x)
                 }
             }
 
-            // refine based on the slope of component i only if the
-            // range of s is greater than a fraction 'min_range' of max
-            // |s|. This eliminates components that consist of small
-            // fluctuations on a constant slope background.
-            if ((smax - smin) > m_min_range*ss) {
-                // maximum allowable difference in slope between
-                // adjacent points.
-                double dmax = m_curve*(smax - smin);
+            // refine based on the slope of component i only if the range of s is
+            // greater than a fraction 'min_range' of max|s|. This eliminates
+            // components that consist of small fluctuations on a constant slope
+            // background.
+            if ((slope_max - slope_min) > m_min_range*slope_amplitude) {
+                // maximum allowable difference in slope between adjacent points.
+                double max_change = m_curve*(slope_max - slope_min) + m_thresh;
                 for (size_t j = 0; j < n-2; j++) {
-                    double r = fabs(s[j+1] - s[j]) / (dmax + m_thresh/dz[j]);
-                    if (r > 1.0 && dz[j] >= 2 * m_gridmin &&
-                            dz[j+1] >= 2 * m_gridmin) {
-                        m_c.insert(name);
-                        m_loc.insert(j);
-                        m_loc.insert(j+1);
+                    double ratio = fabs(slope[j+1] - slope[j]) / max_change;
+                    if (ratio > 1.0 && dz[j] >= 2*m_gridmin && dz[j+1] >= 2*m_gridmin) {
+                        m_component_name.insert(name);
+                        m_insertion_points.insert(j);
+                        m_insertion_points.insert(j+1);
                     }
-                    if (r >= m_prune) {
+                    if (ratio >= m_prune) {
                         m_keep[j+1] = 1;
                     } else if (m_keep[j+1] == 0) {
                         m_keep[j+1] = -1;
@@ -144,14 +144,13 @@ int Refiner::analyze(size_t n, const double* z, const double* x)
         }
     }
 
-    Flow1D* fflame = dynamic_cast<Flow1D*>(m_domain);
-
     // Refine based on properties of the grid itself
     for (size_t j = 1; j < n-1; j++) {
-        // Add a new point if the ratio with left interval is too large
+        // Add a new point if the ratio with left interval is too large.
+        // Extra points around the interval set under consideration are kept.
         if (dz[j] > m_ratio*dz[j-1]) {
-            m_loc.insert(j);
-            m_c.insert(fmt::format("point {}", j));
+            m_insertion_points.insert(j);
+            m_component_name.insert(fmt::format("point {}", j));
             m_keep[j-1] = 1;
             m_keep[j] = 1;
             m_keep[j+1] = 1;
@@ -159,27 +158,28 @@ int Refiner::analyze(size_t n, const double* z, const double* x)
         }
 
         // Add a point if the ratio with right interval is too large
-        if (dz[j] < dz[j-1]/m_ratio) {
-            m_loc.insert(j-1);
-            m_c.insert(fmt::format("point {}", j-1));
+        if (dz[j-1] > m_ratio*dz[j]) {
+            m_insertion_points.insert(j-1);
+            m_component_name.insert(fmt::format("point {}", j-1));
             m_keep[j-2] = 1;
             m_keep[j-1] = 1;
             m_keep[j] = 1;
             m_keep[j+1] = 1;
         }
 
-        // Keep the point if removing would make the ratio with the left
-        // interval too large.
+        // Keep the point if removing would make the ratio with the left interval too
+        // large.
         if (j > 1 && z[j+1]-z[j-1] > m_ratio * dz[j-2]) {
             m_keep[j] = 1;
         }
 
-        // Keep the point if removing would make the ratio with the right
-        // interval too large.
+        // Keep the point if removing would make the ratio with the right interval too
+        // large.
         if (j < n-2 && z[j+1]-z[j-1] > m_ratio * dz[j+1]) {
             m_keep[j] = 1;
         }
 
+        Flow1D* fflame = dynamic_cast<Flow1D*>(m_domain);
         // Keep the point where the temperature is fixed
         if (fflame && fflame->isFree() && z[j] == fflame->m_zfixed) {
             m_keep[j] = 1;
@@ -192,7 +192,6 @@ int Refiner::analyze(size_t n, const double* z, const double* x)
         {
             m_keep[j] = 1;
         }
-
     }
 
     // Don't allow pruning to remove multiple adjacent grid points
@@ -203,27 +202,27 @@ int Refiner::analyze(size_t n, const double* z, const double* x)
         }
     }
 
-    return int(m_loc.size());
+    return int(m_insertion_points.size());
 }
 
-double Refiner::value(const double* x, size_t i, size_t j)
+double Refiner::value(const double* x, size_t n, size_t j)
 {
-    return x[m_domain->index(i,j)];
+    return x[m_domain->index(n,j)];
 }
 
 void Refiner::show()
 {
-    if (!m_loc.empty()) {
+    if (!m_insertion_points.empty()) {
         writeline('#', 78);
         writelog(string("Refining grid in ") +
                  m_domain->id()+".\n"
                  +"    New points inserted after grid points ");
-        for (const auto& loc : m_loc) {
+        for (const auto& loc : m_insertion_points) {
             writelog("{} ", loc);
         }
         writelog("\n");
         writelog("    to resolve ");
-        for (const auto& c : m_c) {
+        for (const auto& c : m_component_name) {
             writelog(c + " ");
         }
         writelog("\n");
@@ -235,12 +234,12 @@ void Refiner::show()
 
 int Refiner::getNewGrid(int n, const double* z, int nn, double* zn)
 {
-    int nnew = static_cast<int>(m_loc.size());
+    int nnew = static_cast<int>(m_insertion_points.size());
     if (nnew + n > nn) {
         throw CanteraError("Refine::getNewGrid", "array size too small.");
     }
 
-    if (m_loc.empty()) {
+    if (m_insertion_points.empty()) {
         copy(z, z + n, zn);
         return 0;
     }
@@ -249,7 +248,7 @@ int Refiner::getNewGrid(int n, const double* z, int nn, double* zn)
     for (int j = 0; j < n - 1; j++) {
         zn[jn] = z[j];
         jn++;
-        if (m_loc.count(j)) {
+        if (m_insertion_points.count(j)) {
             zn[jn] = 0.5*(z[j] + z[j+1]);
             jn++;
         }

JacobDannecker/cantera_ma

src/oneD/refine.cpp
@@ -50,24 +50,27 @@ int Refiner::analyze(size_t n, const double* z, const double* x)
         return 0;
     }
 
-    m_loc.clear();
-    m_c.clear();
+    // check consistency
+    if (n != m_domain->nPoints()) {
+        throw CanteraError("Refiner::analyze", "number of grid points provided does not match domain size.");
+    }
+
+    // Reset the state of the refiner
+    m_insertion_points.clear();
+    m_component_name.clear();
     m_keep.clear();
 
+    // Keep the first and last grid points
     m_keep[0] = 1;
     m_keep[n-1] = 1;
 
     m_nv = m_domain->nComponents();
 
-    // check consistency
-    if (n != m_domain->nPoints()) {
-        throw CanteraError("Refiner::analyze", "inconsistent");
-    }
-
     // find locations where cell size ratio is too large.
-    vector<double> v(n), s(n-1);
+    vector<double> val(n);
+    vector<double> slope(n-1);
 
-    vector<double> dz(n-1);
+    vector<double> dz(n-1); // Store the right-looking grid spacings
     for (size_t j = 0; j < n-1; j++) {
         dz[j] = z[j+1] - z[j];
     }
@@ -77,39 +80,38 @@ int Refiner::analyze(size_t n, const double* z, const double* x)
             string name = m_domain->componentName(i);
             // get component i at all points
             for (size_t j = 0; j < n; j++) {
-                v[j] = value(x, i, j);
+                val[j] = value(x, i, j);
             }
 
             // slope of component i
             for (size_t j = 0; j < n-1; j++) {
-                s[j] = (value(x, i, j+1) - value(x, i, j))/(z[j+1] - z[j]);
+                slope[j] = (val[j+1] - val[j]) / dz[j];
             }
 
-            // find the range of values and slopes
-            double vmin = *min_element(v.begin(), v.end());
-            double vmax = *max_element(v.begin(), v.end());
-            double smin = *min_element(s.begin(), s.end());
-            double smax = *max_element(s.begin(), s.end());
+            // find the range of values and slopes of component i over the domain
+            double val_min = *min_element(val.begin(), val.end());
+            double val_max = *max_element(val.begin(), val.end());
+            double slope_min = *min_element(slope.begin(), slope.end());
+            double slope_max = *max_element(slope.begin(), slope.end());
 
-            // max absolute values of v and s
-            double aa = std::max(fabs(vmax), fabs(vmin));
-            double ss = std::max(fabs(smax), fabs(smin));
+            // max absolute values of val and slope
+            double val_magnitude = std::max(fabs(val_max), fabs(val_min));
+            double slope_magnitude = std::max(fabs(slope_max), fabs(slope_min));
 
-            // refine based on component i only if the range of v is
-            // greater than a fraction 'min_range' of max |v|. This
-            // eliminates components that consist of small fluctuations
-            // on a constant background.
-            if ((vmax - vmin) > m_min_range*aa) {
-                // maximum allowable difference in value between adjacent
-                // points.
-                double dmax = m_slope*(vmax - vmin) + m_thresh;
+            // refine based on component i only if the range of val is greater than a
+            // fraction 'min_range' of max |val|. This eliminates components that
+            // consist of small fluctuations around a constant value.
+            if ((val_max - val_min) > m_min_range*val_magnitude) {
+                // maximum allowable difference in value between adjacent points. Based
+                // on the global min and max values of the component over the domain.
+                double max_change = m_slope*(val_max - val_min) + m_thresh;
                 for (size_t j = 0; j < n-1; j++) {
-                    double r = fabs(v[j+1] - v[j])/dmax;
-                    if (r > 1.0 && dz[j] >= 2 * m_gridmin) {
-                        m_loc.insert(j);
-                        m_c.insert(name);
+                    double ratio = fabs(val[j+1] - val[j]) / max_change;
+                    if (ratio > 1.0 && dz[j] >= 2 * m_gridmin) {
+                        m_insertion_points.insert(j);
+                        m_component_name.insert(name);
                     }
-                    if (r >= m_prune) {
+                    if (ratio >= m_prune) {
                         m_keep[j] = 1;
                         m_keep[j+1] = 1;
                     } else if (m_keep[j] == 0) {
@@ -118,23 +120,21 @@ int Refiner::analyze(size_t n, const double* z, const double* x)
                 }
             }
 
-            // refine based on the slope of component i only if the
-            // range of s is greater than a fraction 'min_range' of max
-            // |s|. This eliminates components that consist of small
-            // fluctuations on a constant slope background.
-            if ((smax - smin) > m_min_range*ss) {
-                // maximum allowable difference in slope between
-                // adjacent points.
-                double dmax = m_curve*(smax - smin);
+            // refine based on the slope of component i only if the range of s is
+            // greater than a fraction 'min_range' of max|s|. This eliminates
+            // components that consist of small fluctuations on a constant slope
+            // background.
+            if ((slope_max - slope_min) > m_min_range*slope_magnitude) {
+                // maximum allowable difference in slope between adjacent points.
+                double max_change = m_curve*(slope_max - slope_min) + m_thresh;
                 for (size_t j = 0; j < n-2; j++) {
-                    double r = fabs(s[j+1] - s[j]) / (dmax + m_thresh/dz[j]);
-                    if (r > 1.0 && dz[j] >= 2 * m_gridmin &&
-                            dz[j+1] >= 2 * m_gridmin) {
-                        m_c.insert(name);
-                        m_loc.insert(j);
-                        m_loc.insert(j+1);
+                    double ratio = fabs(slope[j+1] - slope[j]) / max_change;
+                    if (ratio > 1.0 && dz[j] >= 2*m_gridmin && dz[j+1] >= 2*m_gridmin) {
+                        m_component_name.insert(name);
+                        m_insertion_points.insert(j);
+                        m_insertion_points.insert(j+1);
                     }
-                    if (r >= m_prune) {
+                    if (ratio >= m_prune) {
                         m_keep[j+1] = 1;
                     } else if (m_keep[j+1] == 0) {
                         m_keep[j+1] = -1;
@@ -144,14 +144,13 @@ int Refiner::analyze(size_t n, const double* z, const double* x)
         }
     }
 
-    Flow1D* fflame = dynamic_cast<Flow1D*>(m_domain);
-
     // Refine based on properties of the grid itself
     for (size_t j = 1; j < n-1; j++) {
-        // Add a new point if the ratio with left interval is too large
+        // Add a new point if the ratio with left interval is too large.
+        // Extra points around the interval set under consideration are kept.
         if (dz[j] > m_ratio*dz[j-1]) {
-            m_loc.insert(j);
-            m_c.insert(fmt::format("point {}", j));
+            m_insertion_points.insert(j);
+            m_component_name.insert(fmt::format("point {}", j));
             m_keep[j-1] = 1;
             m_keep[j] = 1;
             m_keep[j+1] = 1;
@@ -159,27 +158,28 @@ int Refiner::analyze(size_t n, const double* z, const double* x)
         }
 
         // Add a point if the ratio with right interval is too large
-        if (dz[j] < dz[j-1]/m_ratio) {
-            m_loc.insert(j-1);
-            m_c.insert(fmt::format("point {}", j-1));
+        if (dz[j-1] > m_ratio*dz[j]) {
+            m_insertion_points.insert(j-1);
+            m_component_name.insert(fmt::format("point {}", j-1));
             m_keep[j-2] = 1;
             m_keep[j-1] = 1;
             m_keep[j] = 1;
             m_keep[j+1] = 1;
         }
 
-        // Keep the point if removing would make the ratio with the left
-        // interval too large.
+        // Keep the point if removing would make the ratio with the left interval too
+        // large.
         if (j > 1 && z[j+1]-z[j-1] > m_ratio * dz[j-2]) {
             m_keep[j] = 1;
         }
 
-        // Keep the point if removing would make the ratio with the right
-        // interval too large.
+        // Keep the point if removing would make the ratio with the right interval too
+        // large.
         if (j < n-2 && z[j+1]-z[j-1] > m_ratio * dz[j+1]) {
             m_keep[j] = 1;
         }
 
+        Flow1D* fflame = dynamic_cast<Flow1D*>(m_domain);
         // Keep the point where the temperature is fixed
         if (fflame && fflame->isFree() && z[j] == fflame->m_zfixed) {
             m_keep[j] = 1;
@@ -192,7 +192,6 @@ int Refiner::analyze(size_t n, const double* z, const double* x)
         {
             m_keep[j] = 1;
         }
-
     }
 
     // Don't allow pruning to remove multiple adjacent grid points
@@ -203,27 +202,27 @@ int Refiner::analyze(size_t n, const double* z, const double* x)
         }
     }
 
-    return int(m_loc.size());
+    return int(m_insertion_points.size());
 }
 
-double Refiner::value(const double* x, size_t i, size_t j)
+double Refiner::value(const double* x, size_t n, size_t j)
 {
-    return x[m_domain->index(i,j)];
+    return x[m_domain->index(n,j)];
 }
 
 void Refiner::show()
 {
-    if (!m_loc.empty()) {
+    if (!m_insertion_points.empty()) {
         writeline('#', 78);
         writelog(string("Refining grid in ") +
                  m_domain->id()+".\n"
                  +"    New points inserted after grid points ");
-        for (const auto& loc : m_loc) {
+        for (const auto& loc : m_insertion_points) {
             writelog("{} ", loc);
         }
         writelog("\n");
         writelog("    to resolve ");
-        for (const auto& c : m_c) {
+        for (const auto& c : m_component_name) {
             writelog(c + " ");
         }
         writelog("\n");
@@ -235,12 +234,12 @@ void Refiner::show()
 
 int Refiner::getNewGrid(int n, const double* z, int nn, double* zn)
 {
-    int nnew = static_cast<int>(m_loc.size());
+    int nnew = static_cast<int>(m_insertion_points.size());
     if (nnew + n > nn) {
         throw CanteraError("Refine::getNewGrid", "array size too small.");
     }
 
-    if (m_loc.empty()) {
+    if (m_insertion_points.empty()) {
         copy(z, z + n, zn);
         return 0;
     }
@@ -249,7 +248,7 @@ int Refiner::getNewGrid(int n, const double* z, int nn, double* zn)
     for (int j = 0; j < n - 1; j++) {
         zn[jn] = z[j];
         jn++;
-        if (m_loc.count(j)) {
+        if (m_insertion_points.count(j)) {
             zn[jn] = 0.5*(z[j] + z[j+1]);
             jn++;
         }