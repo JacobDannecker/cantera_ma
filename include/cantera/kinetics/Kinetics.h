BangShiuh/cantera

[plasma] make changing reaction and rate update interpolated cross sections

include/cantera/kinetics/Kinetics.h
@@ -1407,6 +1407,26 @@ public:
         return m_root.lock();
     }
 
+    //! Register a function to be called if reaction is changed.
+    //! @param id  A unique ID corresponding to the object affected by the callback.
+    //!   Typically, this is a pointer to an object that also holds a reference to the
+    //!   Kinetics object.
+    //! @param callback  The callback function to be called after any reaction is changed.
+    //! When the callback becomes invalid (for example, the corresponding object is
+    //! being deleted, the removeReactionChangedCallback() method must be invoked.
+    //! @since New in %Cantera 3.1
+    void registerReactionChangedCallback(void* id, const function<void()>& callback)
+    {
+        m_reactionChangedCallbacks[id] = callback;
+    }
+
+    //! Remove the reaction-changed callback function associated with the specified object.
+    //! @since New in %Cantera 3.1
+    void removeReactionChangedCallback(void* id)
+    {
+        m_reactionChangedCallbacks.erase(id);
+    }
+
 protected:
     //! Cache for saved calculations within each Kinetics object.
     ValueCache m_cache;
@@ -1530,6 +1550,9 @@ protected:
 
     //! reference to Solution
     std::weak_ptr<Solution> m_root;
+
+    //! Callback functions that are invoked when the reaction is changed.
+    map<void*, function<void()>> m_reactionChangedCallbacks;
 };
 
 }

src/kinetics/Kinetics.cpp
@@ -703,6 +703,10 @@ bool Kinetics::addReaction(shared_ptr<Reaction> r, bool resize)
         m_ready = false;
     }
 
+    for (const auto& [id, callback] : m_reactionChangedCallbacks) {
+        callback();
+    }
+
     return true;
 }
 
@@ -710,6 +714,13 @@ void Kinetics::modifyReaction(size_t i, shared_ptr<Reaction> rNew)
 {
     checkReactionIndex(i);
     shared_ptr<Reaction>& rOld = m_reactions[i];
+
+    if (rNew->rate()->type() == "electron-collision-plasma") {
+        throw CanteraError("Kinetics::modifyReaction",
+            "Type electron-collision-plasma is not supported. "
+            "Use the rate object of the reaction to modify the data.");
+    }
+
     if (rNew->type() != rOld->type()) {
         throw CanteraError("Kinetics::modifyReaction",
             "Reaction types are different: {} != {}.",
@@ -734,6 +745,7 @@ void Kinetics::modifyReaction(size_t i, shared_ptr<Reaction> rNew)
             rOld->productString(), rNew->productString());
     }
     m_reactions[i] = rNew;
+
     invalidateCache();
 }
 

JacobDannecker/cantera_ma

include/cantera/kinetics/Kinetics.h
@@ -1394,6 +1394,26 @@ public:
         return m_root.lock();
     }
 
+    //! Register a function to be called if reaction is added.
+    //! @param id  A unique ID corresponding to the object affected by the callback.
+    //!   Typically, this is a pointer to an object that also holds a reference to the
+    //!   Kinetics object.
+    //! @param callback  The callback function to be called after any reaction is added.
+    //! When the callback becomes invalid (for example, the corresponding object is
+    //! being deleted, the removeReactionAddedCallback() method must be invoked.
+    //! @since New in %Cantera 3.1
+    void registerReactionAddedCallback(void* id, const function<void()>& callback)
+    {
+        m_reactionAddedCallbacks[id] = callback;
+    }
+
+    //! Remove the reaction-changed callback function associated with the specified object.
+    //! @since New in %Cantera 3.1
+    void removeReactionAddedCallback(void* id)
+    {
+        m_reactionAddedCallbacks.erase(id);
+    }
+
 protected:
     //! Cache for saved calculations within each Kinetics object.
     ValueCache m_cache;
@@ -1517,6 +1537,9 @@ protected:
 
     //! reference to Solution
     std::weak_ptr<Solution> m_root;
+
+    //! Callback functions that are invoked when the reaction is added.
+    map<void*, function<void()>> m_reactionAddedCallbacks;
 };
 
 }

src/kinetics/Kinetics.cpp
@@ -696,6 +696,10 @@ bool Kinetics::addReaction(shared_ptr<Reaction> r, bool resize)
         m_ready = false;
     }
 
+    for (const auto& [id, callback] : m_reactionAddedCallbacks) {
+        callback();
+    }
+
     return true;
 }
 
@@ -703,6 +707,13 @@ void Kinetics::modifyReaction(size_t i, shared_ptr<Reaction> rNew)
 {
     checkReactionIndex(i);
     shared_ptr<Reaction>& rOld = m_reactions[i];
+
+    if (rNew->rate()->type() == "electron-collision-plasma") {
+        throw CanteraError("Kinetics::modifyReaction",
+            "Type electron-collision-plasma is not supported. "
+            "Use the rate object of the reaction to modify the data.");
+    }
+
     if (rNew->type() != rOld->type()) {
         throw CanteraError("Kinetics::modifyReaction",
             "Reaction types are different: {} != {}.",