burkenyo/cantera

[.NET] Use automatic checking of return codes at the interop boundary
Since I am levarging P/Invoke source generation, I can add a custom marshaller for return parameters that check the return and throw a CanteraException as necessary. This removes the need to call CheckReturn out of return parameter marshallers.

interfaces/dotnet/Cantera.Tests/src/ApplicationTest.cs
@@ -136,7 +136,7 @@ public class ApplicationTest
     /// </summary>
     static void ProduceRealLogOutput(string message)
     {
-        InteropUtil.CheckReturn(LibCantera.ct3_writeLog(message + "\n"));
+        LibCantera.ct3_writeLog(message + "\n");
     }
 
     /// <summary>

interfaces/dotnet/Cantera/src/Application.cs
@@ -50,8 +50,7 @@ public static class Application
             }
         };
 
-        InteropUtil.CheckReturn(
-            LibCantera.ct3_setLogCallback(s_invokeMessageLoggedDelegate));
+        LibCantera.ct3_setLogCallback(s_invokeMessageLoggedDelegate);
     }
 
     /// <summary>

interfaces/dotnet/Cantera/src/DataDirectoryCollection.cs
@@ -45,7 +45,7 @@ public class DataDirectoryCollection : IReadOnlyList<DirectoryInfo>
     /// <inheritdoc cref="Add(string)"/>
     public void Add(DirectoryInfo dir)
     {
-        InteropUtil.CheckReturn(LibCantera.ct3_addCanteraDirectory(dir.FullName));
+        LibCantera.ct3_addCanteraDirectory(dir.FullName);
 
         _dirs.Clear();
         _dirs.AddRange(GetDirs());

interfaces/dotnet/Cantera/src/SpeciesCollection.cs
@@ -48,13 +48,7 @@ public class SpeciesCollection : IReadOnlyList<Species>
         get => InteropUtil.GetDoubles(_handle, _species.Count,
             LibCantera.thermo3_getMassFractions);
 
-        set
-        {
-            var retval = LibCantera.thermo3_setMassFractions(_handle,
-                value.Length, value);
-
-            InteropUtil.CheckReturn(retval);
-        }
+        set => LibCantera.thermo3_setMassFractions(_handle,  value.Length, value);
     }
 
     /// <summary>
@@ -67,20 +61,14 @@ public class SpeciesCollection : IReadOnlyList<Species>
         get => InteropUtil.GetDoubles(_handle, _species.Count,
             LibCantera.thermo3_getMoleFractions);
 
-        set
-        {
-            var retval = LibCantera.thermo3_setMoleFractions(_handle,
-                value.Length, value);
-
-            InteropUtil.CheckReturn(retval);
-        }
+        set => LibCantera.thermo3_setMoleFractions(_handle, value.Length, value);
     }
 
     internal SpeciesCollection(ThermoPhaseHandle handle)
     {
         _handle = handle;
 
-        var count = InteropUtil.CheckReturn(LibCantera.thermo3_nSpecies(handle));
+        var count = LibCantera.thermo3_nSpecies(handle);
 
         var pool = MemoryPool<double>.Shared;
 

interfaces/dotnet/Cantera/src/ThermoPhase.cs
@@ -72,10 +72,8 @@ public partial class ThermoPhase
     {
         var interopString = thermoPair.ToInteropString();
 
-        var retVal = LibCantera.thermo3_equilibrate(_handle, interopString, solver,
+        LibCantera.thermo3_equilibrate(_handle, interopString, solver,
             tolerance, maxSteps, maxIterations, logVerbosity);
-
-        InteropUtil.CheckReturn(retVal);
     }
 
     /// <summary>
@@ -88,7 +86,7 @@ public partial class ThermoPhase
             throw new InvalidOperationException($"Cannot set thermo pair {pair}!");
         }
 
-        InteropUtil.CheckReturn(setter(_handle, first, second));
+        setter(_handle, first, second);
     }
 
     partial void ExtraDispose()

JacobDannecker/cantera_ma

interfaces/dotnet/Cantera.Tests/src/ApplicationTest.cs
@@ -135,7 +135,7 @@ public class ApplicationTest
     /// </summary>
     static void ProduceRealLogOutput(string message)
     {
-        InteropUtil.CheckReturn(LibCantera.ct_writeLog(message + "\n"));
+        LibCantera.ct_writeLog(message + "\n");
     }
 
     /// <summary>

interfaces/dotnet/Cantera/src/Application.cs
@@ -50,8 +50,7 @@ public static class Application
             }
         };
 
-        InteropUtil.CheckReturn(
-            LibCantera.ct_setLogCallback(s_invokeMessageLoggedDelegate));
+        LibCantera.ct_setLogCallback(s_invokeMessageLoggedDelegate);
     }
 
     /// <summary>

interfaces/dotnet/Cantera/src/DataDirectoryCollection.cs
@@ -45,7 +45,7 @@ public class DataDirectoryCollection : IReadOnlyList<DirectoryInfo>
     /// <inheritdoc cref="Add(string)"/>
     public void Add(DirectoryInfo dir)
     {
-        InteropUtil.CheckReturn(LibCantera.ct_addDataDirectory(dir.FullName));
+        LibCantera.ct_addDataDirectory(dir.FullName);
 
         _dirs.Clear();
         _dirs.AddRange(GetDirs());

interfaces/dotnet/Cantera/src/SpeciesCollection.cs
@@ -48,13 +48,7 @@ public class SpeciesCollection : IReadOnlyList<Species>
         get => InteropUtil.GetDoubles(_handle, _species.Count,
             LibCantera.thermo_getMassFractions);
 
-        set
-        {
-            var retval = LibCantera.thermo_setMassFractions(_handle,
-                value.Length, value);
-
-            InteropUtil.CheckReturn(retval);
-        }
+        set => LibCantera.thermo_setMassFractions(_handle,  value.Length, value);
     }
 
     /// <summary>
@@ -67,20 +61,14 @@ public class SpeciesCollection : IReadOnlyList<Species>
         get => InteropUtil.GetDoubles(_handle, _species.Count,
             LibCantera.thermo_getMoleFractions);
 
-        set
-        {
-            var retval = LibCantera.thermo_setMoleFractions(_handle,
-                value.Length, value);
-
-            InteropUtil.CheckReturn(retval);
-        }
+        set => LibCantera.thermo_setMoleFractions(_handle, value.Length, value);
     }
 
     internal SpeciesCollection(ThermoPhaseHandle handle)
     {
         _handle = handle;
 
-        var count = InteropUtil.CheckReturn(LibCantera.thermo_nSpecies(handle));
+        var count = LibCantera.thermo_nSpecies(handle);
 
         var pool = MemoryPool<double>.Shared;
 

interfaces/dotnet/Cantera/src/ThermoPhase.cs
@@ -72,10 +72,8 @@ public partial class ThermoPhase
     {
         var interopString = thermoPair.ToInteropString();
 
-        var retVal = LibCantera.thermo_equilibrate(_handle, interopString, solver,
+        LibCantera.thermo_equilibrate(_handle, interopString, solver,
             tolerance, maxSteps, maxIterations, logVerbosity);
-
-        InteropUtil.CheckReturn(retVal);
     }
 
     /// <summary>
@@ -88,7 +86,7 @@ public partial class ThermoPhase
             throw new InvalidOperationException($"Cannot set thermo pair {pair}!");
         }
 
-        InteropUtil.CheckReturn(setter(_handle, first, second));
+        setter(_handle, first, second);
     }
 
     partial void ExtraDispose()