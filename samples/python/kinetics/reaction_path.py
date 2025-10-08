Cantera/cantera

[Reactor] Make cloning of Solution objects optional

samples/python/kinetics/reaction_path.py
@@ -26,7 +26,7 @@ import cantera as ct
 # an object of a class derived from class Kinetics in some state.
 gas = ct.Solution('gri30.yaml')
 gas.TPX = 1300.0, ct.one_atm, 'CH4:0.4, O2:1, N2:3.76'
-r = ct.IdealGasReactor(gas)
+r = ct.IdealGasReactor(gas, clone=True)
 net = ct.ReactorNet([r])
 T = r.T
 while T < 1900:

samples/python/kinetics/shock_tube.py
@@ -52,7 +52,7 @@ for k, m in enumerate(models):
     gas = ct.Solution(file, name=models[m])
     X = {'H2O2':X_H2O2, 'H2O':X_H2O, 'O2':X_O2, 'CO2':X_CO2, 'AR':X_Ar}
     gas.TPX = 1196, 2.127*ct.one_atm, X
-    r = ct.Reactor(contents=gas,energy="on")
+    r = ct.Reactor(contents=gas, energy="on", clone=True)
     reactorNetwork = ct.ReactorNet([r])
     timeHistory = ct.SolutionArray(gas, extra=['t'])
     estIgnitDelay = 1

samples/python/reactors/preconditioned_integration.py
@@ -27,7 +27,7 @@ def integrate_reactor(preconditioner=True):
     gas = ct.Solution('example_data/n-hexane-NUIG-2015.yaml')
     gas.TP = 1000, ct.one_atm
     gas.set_equivalence_ratio(1, 'NC6H14', 'N2:3.76, O2:1.0')
-    reactor = ct.IdealGasConstPressureMoleReactor(gas)
+    reactor = ct.IdealGasConstPressureMoleReactor(gas, clone=True)
     # set volume for reactors
     reactor.volume = 0.1
     # Create reactor network

samples/python/reactors/reactor1.py
@@ -13,7 +13,7 @@ import cantera as ct
 
 gas = ct.Solution('h2o2.yaml')
 gas.TPX = 1001.0, ct.one_atm, 'H2:2,O2:1,N2:4'
-r = ct.IdealGasConstPressureReactor(gas)
+r = ct.IdealGasConstPressureReactor(gas, clone=True)
 
 sim = ct.ReactorNet([r])
 sim.verbose = True

JacobDannecker/cantera_ma

samples/python/kinetics/reaction_path.py
@@ -26,7 +26,7 @@ import cantera as ct
 # an object of a class derived from class Kinetics in some state.
 gas = ct.Solution('gri30.yaml')
 gas.TPX = 1300.0, ct.one_atm, 'CH4:0.4, O2:1, N2:3.76'
-r = ct.IdealGasReactor(gas)
+r = ct.IdealGasReactor(gas, clone=False)
 net = ct.ReactorNet([r])
 T = r.T
 while T < 1900:

samples/python/kinetics/shock_tube.py
@@ -52,7 +52,7 @@ for k, m in enumerate(models):
     gas = ct.Solution(file, name=models[m])
     X = {'H2O2':X_H2O2, 'H2O':X_H2O, 'O2':X_O2, 'CO2':X_CO2, 'AR':X_Ar}
     gas.TPX = 1196, 2.127*ct.one_atm, X
-    r = ct.Reactor(contents=gas,energy="on")
+    r = ct.Reactor(contents=gas, energy="on", clone=False)
     reactorNetwork = ct.ReactorNet([r])
     timeHistory = ct.SolutionArray(gas, extra=['t'])
     estIgnitDelay = 1

samples/python/reactors/preconditioned_integration.py
@@ -27,7 +27,7 @@ def integrate_reactor(preconditioner=True):
     gas = ct.Solution('example_data/n-hexane-NUIG-2015.yaml')
     gas.TP = 1000, ct.one_atm
     gas.set_equivalence_ratio(1, 'NC6H14', 'N2:3.76, O2:1.0')
-    reactor = ct.IdealGasConstPressureMoleReactor(gas)
+    reactor = ct.IdealGasConstPressureMoleReactor(gas, clone=False)
     # set volume for reactors
     reactor.volume = 0.1
     # Create reactor network

samples/python/reactors/reactor1.py
@@ -13,7 +13,7 @@ import cantera as ct
 
 gas = ct.Solution('h2o2.yaml')
 gas.TPX = 1001.0, ct.one_atm, 'H2:2,O2:1,N2:4'
-r = ct.IdealGasConstPressureReactor(gas)
+r = ct.IdealGasConstPressureReactor(gas, clone=False)
 
 sim = ct.ReactorNet([r])
 sim.verbose = True