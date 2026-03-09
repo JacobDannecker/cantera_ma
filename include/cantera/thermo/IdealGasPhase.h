Cantera/cantera

[Thermo] Implement constant TV thermo properties for Redlich-Kwong
Providing these properties allows the Redlich-Kwong model to be used
with the preconditioner for the temperature-based IdealGasMoleReactor.

include/cantera/thermo/IdealGasPhase.h
@@ -484,7 +484,9 @@ public:
     void getPartialMolarEnthalpies(span<double> hbar) const override;
     void getPartialMolarEntropies(span<double> sbar) const override;
     void getPartialMolarIntEnergies(span<double> ubar) const override;
+    void getPartialMolarIntEnergies_TV(span<double> ubar) const override;
     void getPartialMolarCp(span<double> cpbar) const override;
+    void getPartialMolarCv_TV(span<double> cvbar) const override;
     void getPartialMolarVolumes(span<double> vbar) const override;
 
     //! @}

include/cantera/thermo/ThermoPhase.h
@@ -890,6 +890,22 @@ public:
                                   "Not implemented for phase type '{}'", type());
     }
 
+    //! Return an array of partial molar internal energies at constant temperature
+    //! and volume [J/kmol].
+    /*!
+     * This returns the constant-volume species derivatives of the internal energy:
+     * @f[
+     * \tilde{u}_k = \left(\partial U / \partial n_k\right)_{T,V,n_{j\ne k}}.
+     * @f]
+     * These quantities are distinct from the partial molar internal energies
+     * @f$ \bar{u}_k = \left(\partial U / \partial n_k\right)_{T,P,n_{j\ne k}} @f$.
+     * which are defined at constant pressure.
+     */
+    virtual void getPartialMolarIntEnergies_TV(span<double> ubar) const {
+        throw NotImplementedError("ThermoPhase::getPartialMolarIntEnergies_TV",
+                                  "Not implemented for phase type '{}'", type());
+    }
+
     //! Return an array of partial molar heat capacities for the
     //! species in the mixture.  Units: J/kmol/K
     /*!
@@ -899,6 +915,21 @@ public:
      */
     virtual void getPartialMolarCp(span<double> cpbar) const {
         throw NotImplementedError("ThermoPhase::getPartialMolarCp",
+                                  "Not implemented for phase type '{}'", type());
+    }
+
+    //! Return an array of species molar heat capacities associated with the
+    //! constant-volume partial molar internal energies [J/kmol/K].
+    /*!
+     * This returns
+     * @f[
+     * \tilde{c}_{v,k} = \left(\partial \tilde{u}_k / \partial T\right)_{V,n}
+     * @f]
+     * for each species *k* where @f$ \tilde{u}_k @f$ are the constant-volume partial
+     * molar internal energies computed by getPartialMolarIntEnergies_TV().
+     */
+    virtual void getPartialMolarCv_TV(span<double> cvbar) const {
+        throw NotImplementedError("ThermoPhase::getPartialMolarCv_TV",
                                   "Not implemented for phase type '{}'", type());
     }
 

src/thermo/IdealGasPhase.cpp
@@ -104,11 +104,25 @@ void IdealGasPhase::getPartialMolarIntEnergies(span<double> ubar) const
     }
 }
 
+void IdealGasPhase::getPartialMolarIntEnergies_TV(span<double> ubar) const
+{
+    getPartialMolarIntEnergies(ubar);
+}
+
 void IdealGasPhase::getPartialMolarCp(span<double> cpbar) const
 {
     checkArraySize("IdealGasPhase::getPartialMolarCp", cpbar.size(), m_kk);
     auto _cp = cp_R_ref();
     scale(_cp.begin(), _cp.end(), cpbar.begin(), GasConstant);
+}
+
+void IdealGasPhase::getPartialMolarCv_TV(span<double> cvbar) const
+{
+    checkArraySize("IdealGasPhase::getPartialMolarCv_TV", cvbar.size(), m_kk);
+    auto _cp = cp_R_ref();
+    for (size_t k = 0; k < m_kk; k++) {
+        cvbar[k] = GasConstant * (_cp[k] - 1.0);
+    }
 }
 
 void IdealGasPhase::getPartialMolarVolumes(span<double> vbar) const

src/thermo/RedlichKwongMFTP.cpp
@@ -303,6 +303,195 @@ void RedlichKwongMFTP::getPartialMolarIntEnergies(span<double> ubar) const
     }
 }
 
+void RedlichKwongMFTP::getPartialMolarIntEnergies_TV(span<double> ubar) const
+{
+    checkArraySize("RedlichKwongMFTP::getPartialMolarIntEnergies_TV", ubar.size(), m_kk);
+    _updateReferenceStateThermo();
+
+    double T = temperature();
+    double sqt = sqrt(T);
+    double mv = molarVolume();
+    double b = m_b_current;
+    double a = m_a_current;
+    double dadt = da_dt();
+
+    for (size_t k = 0; k < m_kk; k++) {
+        ubar[k] = RT() * (m_h0_RT[k] - 1.0);
+    }
+
+    if (fabs(b) < SmallNumber) {
+        return;
+    }
+
+    // A_k = sum_i x_i a_ki and A'_k = dA_k/dT = sum_i x_i a_ki,1
+    for (size_t k = 0; k < m_kk; k++) {
+        m_pp[k] = 0.0;
+        m_dAkdT[k] = 0.0;
+        for (size_t i = 0; i < m_kk; i++) {
+            size_t counter = k + m_kk * i;
+            m_pp[k] += moleFractions_[i] * a_vec_Curr_[counter];
+            m_dAkdT[k] += moleFractions_[i] * a_coeff_vec(1, counter);
+        }
+    }
+
+    double vpb = mv + b;
+    double logv = log(vpb / mv);
+    double F = T * dadt - 1.5 * a;
+    double C = -logv / b + 1.0 / vpb;
+    double pref = 1.0 / (b * sqt);
+
+    for (size_t k = 0; k < m_kk; k++) {
+        double Sk = 2.0 * T * m_dAkdT[k] - 3.0 * m_pp[k];
+        double ures = pref * (logv * Sk + b_vec_Curr_[k] * F * C);
+        ubar[k] += ures;
+    }
+}
+
+void RedlichKwongMFTP::getPartialMolarCp(span<double> cpbar) const
+{
+    checkArraySize("RedlichKwongMFTP::getPartialMolarCp", cpbar.size(), m_kk);
+    _updateReferenceStateThermo();
+
+    double T = temperature();
+    double sqt = sqrt(T);
+    double v = molarVolume();
+    double b = m_b_current;
+    double a = m_a_current;
+    double dadt = da_dt();
+    double d2adt2 = 0.0; // current RK model uses a(T)=a0+a1*T
+
+    for (size_t k = 0; k < m_kk; k++) {
+        cpbar[k] = GasConstant * m_cp0_R[k];
+    }
+
+    if (fabs(b) < SmallNumber) {
+        return;
+    }
+
+    // Mixture pressure derivatives
+    pressureDerivatives();
+    double pT = dpdT_;
+    double pV = dpdV_;
+    double dvdT_P = -pT / pV;
+
+    double vpb = v + b;
+    double vmb = v - b;
+
+    // P_TT, P_TV and P_VV for v''(T)|P from EOS:
+    // v'' = -(P_TT + 2 P_TV v' + P_VV v'^2) / P_V
+    double pTT = (-d2adt2 + dadt / T - 3.0 * a / (4.0 * T * T)) / (sqt * v * vpb);
+    double pTV = -GasConstant / (vmb * vmb)
+        + (dadt - a / (2.0 * T)) * (2.0 * v + b) / (sqt * v * v * vpb * vpb);
+    double pVV = 2.0 * GasConstant * T / (vmb * vmb * vmb)
+        - 2.0 * a * (3.0 * v * v + 3.0 * b * v + b * b)
+            / (sqt * v * v * v * vpb * vpb * vpb);
+    double d2vdT2_P = -(pTT + 2.0 * pTV * dvdT_P + pVV * dvdT_P * dvdT_P) / pV;
+
+    // Species-dependent A_k and dA_k/dT
+    for (size_t k = 0; k < m_kk; k++) {
+        m_pp[k] = 0.0;
+        m_dAkdT[k] = 0.0;
+        for (size_t i = 0; i < m_kk; i++) {
+            size_t counter = k + m_kk * i;
+            m_pp[k] += moleFractions_[i] * a_vec_Curr_[counter];
+            m_dAkdT[k] += moleFractions_[i] * a_coeff_vec(1, counter);
+        }
+    }
+
+    double F = T * dadt - 1.5 * a;
+    double dF_dT = -0.5 * dadt + T * d2adt2;
+    double logvpv = log(vpb / v);
+    double termLPrime = -b * dvdT_P / (v * vpb);
+
+    for (size_t k = 0; k < m_kk; k++) {
+        double bk = b_vec_Curr_[k];
+        double Ak = m_pp[k];
+        double dAk = m_dAkdT[k];
+        double Sk = 2.0 * T * dAk - 3.0 * Ak;
+        double dSk_dT = -dAk; // for linear a(T)
+
+        // dp/dn_k at constant T,V,n_j
+        double dpdni = RT() / vmb + RT() * bk / (vmb * vmb)
+            - 2.0 * Ak / (sqt * v * vpb)
+            + a * bk / (sqt * v * vpb * vpb);
+
+        // d/dT|P [dp/dn_k]
+        double d_dpdni_dT = GasConstant / vmb
+            - GasConstant * T * dvdT_P / (vmb * vmb)
+            + GasConstant * bk / (vmb * vmb)
+            - 2.0 * GasConstant * T * bk * dvdT_P / (vmb * vmb * vmb)
+            - 2.0 * dAk / (sqt * v * vpb)
+            + Ak / (T * sqt * v * vpb)
+            + 2.0 * Ak * (2.0 * v + b) * dvdT_P / (sqt * v * v * vpb * vpb)
+            + bk * dadt / (sqt * v * vpb * vpb)
+            - bk * a / (2.0 * T * sqt * v * vpb * vpb)
+            - bk * a * (3.0 * v + b) * dvdT_P / (sqt * v * v * vpb * vpb * vpb);
+
+        // d/dT|P of departure terms used in getPartialMolarEnthalpies
+        double dterm1 = -bk / (b * b * sqt)
+            * (termLPrime * F + logvpv * dF_dT - logvpv * F / (2.0 * T));
+        double dterm2 = 1.0 / (b * sqt)
+            * (termLPrime * Sk + logvpv * dSk_dT - logvpv * Sk / (2.0 * T));
+        double dterm3 = bk / (b * sqt)
+            * (dF_dT / vpb - F * dvdT_P / (vpb * vpb) - F / (2.0 * T * vpb));
+
+        // cp_k = d hbar_k / dT at constant P and composition
+        cpbar[k] += -GasConstant
+            + (dvdT_P + T * d2vdT2_P) * dpdni
+            + T * dvdT_P * d_dpdni_dT
+            + dterm1 + dterm2 + dterm3;
+    }
+}
+
+void RedlichKwongMFTP::getPartialMolarCv_TV(span<double> cvbar) const
+{
+    checkArraySize("RedlichKwongMFTP::getPartialMolarCv_TV", cvbar.size(), m_kk);
+    _updateReferenceStateThermo();
+
+    double T = temperature();
+    double sqt = sqrt(T);
+    double mv = molarVolume();
+    double b = m_b_current;
+    double a = m_a_current;
+    double dadt = da_dt();
+
+    for (size_t k = 0; k < m_kk; k++) {
+        cvbar[k] = GasConstant * (m_cp0_R[k] - 1.0);
+    }
+
+    if (fabs(b) < SmallNumber) {
+        return;
+    }
+
+    // A_k = sum_i x_i a_ki and A'_k = dA_k/dT = sum_i x_i a_ki,1
+    for (size_t k = 0; k < m_kk; k++) {
+        m_pp[k] = 0.0;
+        m_dAkdT[k] = 0.0;
+        for (size_t i = 0; i < m_kk; i++) {
+            size_t counter = k + m_kk * i;
+            m_pp[k] += moleFractions_[i] * a_vec_Curr_[counter];
+            m_dAkdT[k] += moleFractions_[i] * a_coeff_vec(1, counter);
+        }
+    }
+
+    // Residual contribution for
+    // \tilde{u}_k = (\partial U / \partial n_k)_{T,V,n_j}
+    // with linear a(T): a_ij = a_ij,0 + a_ij,1 T
+    double vpb = mv + b;
+    double logv = log(vpb / mv);
+    double F = T * dadt - 1.5 * a;
+    double C = -logv / b + 1.0 / vpb;
+    double pref = 1.0 / (b * sqt);
+
+    for (size_t k = 0; k < m_kk; k++) {
+        double Sk = 2.0 * T * m_dAkdT[k] - 3.0 * m_pp[k];
+        double ures = pref * (logv * Sk + b_vec_Curr_[k] * F * C);
+        double dresdT = pref * (-logv * m_dAkdT[k] - 0.5 * b_vec_Curr_[k] * dadt * C)
+            - 0.5 * ures / T;
+        cvbar[k] += dresdT;
+    }
+}
+
 void RedlichKwongMFTP::getPartialMolarVolumes(span<double> vbar) const
 {
     checkArraySize("RedlichKwongMFTP::getPartialMolarVolumes", vbar.size(), m_kk);
@@ -349,6 +538,7 @@ bool RedlichKwongMFTP::addSpecies(shared_ptr<Species> spec)
         a_coeff_vec.resize(2, m_kk * m_kk, NAN);
 
         m_pp.push_back(0.0);
+        m_dAkdT.push_back(0.0);
         m_coeffSource.push_back(CoeffSource::EoS);
         m_partialMolarVolumes.push_back(0.0);
         dpdni_.push_back(0.0);

JacobDannecker/cantera_ma

include/cantera/thermo/IdealGasPhase.h
@@ -484,7 +484,9 @@ public:
     void getPartialMolarEnthalpies(span<double> hbar) const override;
     void getPartialMolarEntropies(span<double> sbar) const override;
     void getPartialMolarIntEnergies(span<double> ubar) const override;
+    void getPartialMolarIntEnergies_TV(span<double> utilde) const override;
     void getPartialMolarCp(span<double> cpbar) const override;
+    void getPartialMolarCv_TV(span<double> cvtilde) const override;
     void getPartialMolarVolumes(span<double> vbar) const override;
 
     //! @}

include/cantera/thermo/ThermoPhase.h
@@ -892,6 +892,24 @@ public:
                                   "Not implemented for phase type '{}'", type());
     }
 
+    //! Return an array of partial molar internal energies at constant temperature
+    //! and volume [J/kmol].
+    /*!
+     * This returns the constant-volume species derivatives of the internal energy:
+     * @f[
+     * \tilde{u}_k = \left(\partial U / \partial n_k\right)_{T,V,n_{j\ne k}}.
+     * @f]
+     * These quantities are distinct from the partial molar internal energies
+     * @f$ \bar{u}_k = \left(\partial U / \partial n_k\right)_{T,P,n_{j\ne k}} @f$.
+     * which are defined at constant pressure.
+     *
+     * @since New in %Cantera 4.0.
+     */
+    virtual void getPartialMolarIntEnergies_TV(span<double> utilde) const {
+        throw NotImplementedError("ThermoPhase::getPartialMolarIntEnergies_TV",
+                                  "Not implemented for phase type '{}'", type());
+    }
+
     //! Return an array of partial molar heat capacities for the
     //! species in the mixture.  Units: J/kmol/K
     /*!
@@ -901,6 +919,23 @@ public:
      */
     virtual void getPartialMolarCp(span<double> cpbar) const {
         throw NotImplementedError("ThermoPhase::getPartialMolarCp",
+                                  "Not implemented for phase type '{}'", type());
+    }
+
+    //! Return an array of species molar heat capacities associated with the
+    //! constant-volume partial molar internal energies [J/kmol/K].
+    /*!
+     * This returns
+     * @f[
+     * \tilde{c}_{v,k} = \left(\partial \tilde{u}_k / \partial T\right)_{V,n}
+     * @f]
+     * for each species *k* where @f$ \tilde{u}_k @f$ are the constant-volume partial
+     * molar internal energies computed by getPartialMolarIntEnergies_TV().
+     *
+     * @since New in %Cantera 4.0.
+     */
+    virtual void getPartialMolarCv_TV(span<double> cvtilde) const {
+        throw NotImplementedError("ThermoPhase::getPartialMolarCv_TV",
                                   "Not implemented for phase type '{}'", type());
     }
 

src/thermo/IdealGasPhase.cpp
@@ -104,11 +104,25 @@ void IdealGasPhase::getPartialMolarIntEnergies(span<double> ubar) const
     }
 }
 
+void IdealGasPhase::getPartialMolarIntEnergies_TV(span<double> utilde) const
+{
+    getPartialMolarIntEnergies(utilde);
+}
+
 void IdealGasPhase::getPartialMolarCp(span<double> cpbar) const
 {
     checkArraySize("IdealGasPhase::getPartialMolarCp", cpbar.size(), m_kk);
     auto _cp = cp_R_ref();
     scale(_cp.begin(), _cp.end(), cpbar.begin(), GasConstant);
+}
+
+void IdealGasPhase::getPartialMolarCv_TV(span<double> cvtilde) const
+{
+    checkArraySize("IdealGasPhase::getPartialMolarCv_TV", cvtilde.size(), m_kk);
+    auto _cp = cp_R_ref();
+    for (size_t k = 0; k < m_kk; k++) {
+        cvtilde[k] = GasConstant * (_cp[k] - 1.0);
+    }
 }
 
 void IdealGasPhase::getPartialMolarVolumes(span<double> vbar) const

src/thermo/RedlichKwongMFTP.cpp
@@ -303,6 +303,195 @@ void RedlichKwongMFTP::getPartialMolarIntEnergies(span<double> ubar) const
     }
 }
 
+void RedlichKwongMFTP::getPartialMolarIntEnergies_TV(span<double> utilde) const
+{
+    checkArraySize("RedlichKwongMFTP::getPartialMolarIntEnergies_TV", utilde.size(), m_kk);
+    _updateReferenceStateThermo();
+
+    double T = temperature();
+    double sqt = sqrt(T);
+    double mv = molarVolume();
+    double b = m_b_current;
+    double a = m_a_current;
+    double dadt = da_dt();
+
+    for (size_t k = 0; k < m_kk; k++) {
+        utilde[k] = RT() * (m_h0_RT[k] - 1.0);
+    }
+
+    if (fabs(b) < SmallNumber) {
+        return;
+    }
+
+    // A_k = sum_i x_i a_ki and A'_k = dA_k/dT = sum_i x_i a_ki,1
+    for (size_t k = 0; k < m_kk; k++) {
+        m_pp[k] = 0.0;
+        m_dAkdT[k] = 0.0;
+        for (size_t i = 0; i < m_kk; i++) {
+            size_t counter = k + m_kk * i;
+            m_pp[k] += moleFractions_[i] * a_vec_Curr_[counter];
+            m_dAkdT[k] += moleFractions_[i] * a_coeff_vec(1, counter);
+        }
+    }
+
+    double vpb = mv + b;
+    double logv = log(vpb / mv);
+    double F = T * dadt - 1.5 * a;
+    double C = -logv / b + 1.0 / vpb;
+    double pref = 1.0 / (b * sqt);
+
+    for (size_t k = 0; k < m_kk; k++) {
+        double Sk = 2.0 * T * m_dAkdT[k] - 3.0 * m_pp[k];
+        double ures = pref * (logv * Sk + b_vec_Curr_[k] * F * C);
+        utilde[k] += ures;
+    }
+}
+
+void RedlichKwongMFTP::getPartialMolarCp(span<double> cpbar) const
+{
+    checkArraySize("RedlichKwongMFTP::getPartialMolarCp", cpbar.size(), m_kk);
+    _updateReferenceStateThermo();
+
+    double T = temperature();
+    double sqt = sqrt(T);
+    double v = molarVolume();
+    double b = m_b_current;
+    double a = m_a_current;
+    double dadt = da_dt();
+    double d2adt2 = 0.0; // current RK model uses a(T)=a0+a1*T
+
+    for (size_t k = 0; k < m_kk; k++) {
+        cpbar[k] = GasConstant * m_cp0_R[k];
+    }
+
+    if (fabs(b) < SmallNumber) {
+        return;
+    }
+
+    // Mixture pressure derivatives
+    pressureDerivatives();
+    double pT = dpdT_;
+    double pV = dpdV_;
+    double dvdT_P = -pT / pV;
+
+    double vpb = v + b;
+    double vmb = v - b;
+
+    // P_TT, P_TV and P_VV for v''(T)|P from EOS:
+    // v'' = -(P_TT + 2 P_TV v' + P_VV v'^2) / P_V
+    double pTT = (-d2adt2 + dadt / T - 3.0 * a / (4.0 * T * T)) / (sqt * v * vpb);
+    double pTV = -GasConstant / (vmb * vmb)
+        + (dadt - a / (2.0 * T)) * (2.0 * v + b) / (sqt * v * v * vpb * vpb);
+    double pVV = 2.0 * GasConstant * T / (vmb * vmb * vmb)
+        - 2.0 * a * (3.0 * v * v + 3.0 * b * v + b * b)
+            / (sqt * v * v * v * vpb * vpb * vpb);
+    double d2vdT2_P = -(pTT + 2.0 * pTV * dvdT_P + pVV * dvdT_P * dvdT_P) / pV;
+
+    // Species-dependent A_k and dA_k/dT
+    for (size_t k = 0; k < m_kk; k++) {
+        m_pp[k] = 0.0;
+        m_dAkdT[k] = 0.0;
+        for (size_t i = 0; i < m_kk; i++) {
+            size_t counter = k + m_kk * i;
+            m_pp[k] += moleFractions_[i] * a_vec_Curr_[counter];
+            m_dAkdT[k] += moleFractions_[i] * a_coeff_vec(1, counter);
+        }
+    }
+
+    double F = T * dadt - 1.5 * a;
+    double dF_dT = -0.5 * dadt + T * d2adt2;
+    double logvpv = log(vpb / v);
+    double termLPrime = -b * dvdT_P / (v * vpb);
+
+    for (size_t k = 0; k < m_kk; k++) {
+        double bk = b_vec_Curr_[k];
+        double Ak = m_pp[k];
+        double dAk = m_dAkdT[k];
+        double Sk = 2.0 * T * dAk - 3.0 * Ak;
+        double dSk_dT = -dAk; // for linear a(T)
+
+        // dp/dn_k at constant T,V,n_j
+        double dpdni = RT() / vmb + RT() * bk / (vmb * vmb)
+            - 2.0 * Ak / (sqt * v * vpb)
+            + a * bk / (sqt * v * vpb * vpb);
+
+        // d/dT|P [dp/dn_k]
+        double d_dpdni_dT = GasConstant / vmb
+            - GasConstant * T * dvdT_P / (vmb * vmb)
+            + GasConstant * bk / (vmb * vmb)
+            - 2.0 * GasConstant * T * bk * dvdT_P / (vmb * vmb * vmb)
+            - 2.0 * dAk / (sqt * v * vpb)
+            + Ak / (T * sqt * v * vpb)
+            + 2.0 * Ak * (2.0 * v + b) * dvdT_P / (sqt * v * v * vpb * vpb)
+            + bk * dadt / (sqt * v * vpb * vpb)
+            - bk * a / (2.0 * T * sqt * v * vpb * vpb)
+            - bk * a * (3.0 * v + b) * dvdT_P / (sqt * v * v * vpb * vpb * vpb);
+
+        // d/dT|P of departure terms used in getPartialMolarEnthalpies
+        double dterm1 = -bk / (b * b * sqt)
+            * (termLPrime * F + logvpv * dF_dT - logvpv * F / (2.0 * T));
+        double dterm2 = 1.0 / (b * sqt)
+            * (termLPrime * Sk + logvpv * dSk_dT - logvpv * Sk / (2.0 * T));
+        double dterm3 = bk / (b * sqt)
+            * (dF_dT / vpb - F * dvdT_P / (vpb * vpb) - F / (2.0 * T * vpb));
+
+        // cp_k = d hbar_k / dT at constant P and composition
+        cpbar[k] += -GasConstant
+            + (dvdT_P + T * d2vdT2_P) * dpdni
+            + T * dvdT_P * d_dpdni_dT
+            + dterm1 + dterm2 + dterm3;
+    }
+}
+
+void RedlichKwongMFTP::getPartialMolarCv_TV(span<double> cvtilde) const
+{
+    checkArraySize("RedlichKwongMFTP::getPartialMolarCv_TV", cvtilde.size(), m_kk);
+    _updateReferenceStateThermo();
+
+    double T = temperature();
+    double sqt = sqrt(T);
+    double mv = molarVolume();
+    double b = m_b_current;
+    double a = m_a_current;
+    double dadt = da_dt();
+
+    for (size_t k = 0; k < m_kk; k++) {
+        cvtilde[k] = GasConstant * (m_cp0_R[k] - 1.0);
+    }
+
+    if (fabs(b) < SmallNumber) {
+        return;
+    }
+
+    // A_k = sum_i x_i a_ki and A'_k = dA_k/dT = sum_i x_i a_ki,1
+    for (size_t k = 0; k < m_kk; k++) {
+        m_pp[k] = 0.0;
+        m_dAkdT[k] = 0.0;
+        for (size_t i = 0; i < m_kk; i++) {
+            size_t counter = k + m_kk * i;
+            m_pp[k] += moleFractions_[i] * a_vec_Curr_[counter];
+            m_dAkdT[k] += moleFractions_[i] * a_coeff_vec(1, counter);
+        }
+    }
+
+    // Residual contribution for
+    // \tilde{u}_k = (\partial U / \partial n_k)_{T,V,n_j}
+    // with linear a(T): a_ij = a_ij,0 + a_ij,1 T
+    double vpb = mv + b;
+    double logv = log(vpb / mv);
+    double F = T * dadt - 1.5 * a;
+    double C = -logv / b + 1.0 / vpb;
+    double pref = 1.0 / (b * sqt);
+
+    for (size_t k = 0; k < m_kk; k++) {
+        double Sk = 2.0 * T * m_dAkdT[k] - 3.0 * m_pp[k];
+        double ures = pref * (logv * Sk + b_vec_Curr_[k] * F * C);
+        double dresdT = pref * (-logv * m_dAkdT[k] - 0.5 * b_vec_Curr_[k] * dadt * C)
+            - 0.5 * ures / T;
+        cvtilde[k] += dresdT;
+    }
+}
+
 void RedlichKwongMFTP::getPartialMolarVolumes(span<double> vbar) const
 {
     checkArraySize("RedlichKwongMFTP::getPartialMolarVolumes", vbar.size(), m_kk);
@@ -349,6 +538,7 @@ bool RedlichKwongMFTP::addSpecies(shared_ptr<Species> spec)
         a_coeff_vec.resize(2, m_kk * m_kk, NAN);
 
         m_pp.push_back(0.0);
+        m_dAkdT.push_back(0.0);
         m_coeffSource.push_back(CoeffSource::EoS);
         m_partialMolarVolumes.push_back(0.0);
         dpdni_.push_back(0.0);