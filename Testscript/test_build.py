4796c99a60825316fc688c8ea81d036874f75ac2
JacobDannecker <jacob.dannecker@stud.hs-mannheim.de>
2026-04-07 11:08:20 +0200

    Add python script to test build.

---
+++ Testscript/test_build.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cantera as ct
+from matplotlib import pyplot as plt
+
+reaction_mechanism = 'h2o2.yaml'
+gas = ct.Solution(reaction_mechanism)
+width = 18e-3  
+
+grid = np.linspace(0., width, 200)
+f = ct.CounterflowDiffusionFlame(gas, grid=grid)
+f.P = 1.e5  
+f.fuel_inlet.mdot = 0.5  
+f.fuel_inlet.X = 'H2:1'
+f.fuel_inlet.T = 300 
+f.oxidizer_inlet.mdot = 3.0
+f.oxidizer_inlet.X = 'O2:1'
+f.oxidizer_inlet.T = 300 
+
+f.set_refine_criteria(ratio=3.0, slope=0.1, curve=0.2, prune=0.03)
+
+f.solve(loglevel=1)
+
+print(f.T)
+plt.plot(f.grid, f.T)
+plt.scatter(f.grid[100], f.T[100], color="r")
+plt.show()
