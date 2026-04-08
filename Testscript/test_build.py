d3a8fcaeec8645743a679292fbb93f0e0d14c9dc
JacobDannecker <jacob.dannecker@stud.hs-mannheim.de>
2026-04-08 12:04:45 +0200

    Ad plot in mixture fraction.

--- Testscript/test_build.py
+++ Testscript/test_build.py
@@ -6,7 +6,7 @@ reaction_mechanism = 'h2o2.yaml'
 gas = ct.Solution(reaction_mechanism)
 width = 18e-3  
 
-grid = np.linspace(0., width, 200)
+grid = np.linspace(0., width, 250)
 f = ct.CounterflowDiffusionFlame(gas, grid=grid)
 f.P = 1.e5  
 f.fuel_inlet.mdot = 0.5  
@@ -20,14 +20,32 @@ f.oxidizer_inlet.T = 300
 
 f.solve(loglevel=1)
 f.save("Testscript/Data/TestNewBuild.h5", name="NewBuild", overwrite=True)
+fig, ax = plt.subplots(2, 1)
+strain_new_max = f.strain_rate("max")
 
-fig, ax = plt.subplots()
-ax.plot(f.grid, f.T, label="NewBuild")
-ax.scatter(f.grid[107], f.T[107], color="r")
+ax[0].plot(f.grid, f.T, label=f"NewBuild strain_max: {strain_new_max:.2f}")
+ax[0].scatter(f.grid[100], f.T[100], color="r")
+ax[1].plot(f.mixture_fraction("H"), f.T, label=f"NewBuild strain_max: {strain_new_max:.2f}")
+
+print(f.fuel_inlet.mdot)
+print(f.oxidizer_inlet.mdot)
+
 f.restore("Testscript/Data/TestNewBuild.h5", name="Reference")
-ax.plot(f.grid, f.T, label="Reference")
-ax.grid()
-ax.legend()
-ax.set_ylabel("T")
-ax.set_xlabel("x")
+print(f.fuel_inlet.mdot)
+print(f.oxidizer_inlet.mdot)
+strain_ref_max = f.strain_rate("max")
+
+ax[0].plot(f.grid, f.T, label=f"Reference strain_max: {strain_ref_max:.2f}")
+ax[1].plot(f.mixture_fraction("H"), f.T, label=f"Reference strain_max: {strain_ref_max:.2f}")
+
+ax[1].grid()
+ax[1].legend()
+ax[1].set_ylabel("T")
+ax[1].set_xlabel("<- ox z fuel ->")
+
+ax[0].grid()
+ax[0].legend()
+ax[0].set_ylabel("T")
+ax[0].set_xlabel("<- fuel x ox->")
+
 plt.show()
