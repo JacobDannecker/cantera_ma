ca091111f48af12923c7a5aad680728f6402f4cc
JacobDannecker <jacob.dannecker@stud.hs-mannheim.de>
2026-04-17 10:01:00 +0200

    Change wall_pos parameter from int to double

--- interfaces/cython/cantera/_onedim.pyi
+++ interfaces/cython/cantera/_onedim.pyi
@@ -311,8 +311,8 @@ class Sim1D:
         loglevel: LogLevel = 1,
         refine_grid: bool = True,
         auto: bool = False,
-        wall_pos: int = 12,
-        factor: int = 22,
+        wall_pos: float = 0.1,
+        factor: int = 1000,
     ) -> None: ...
     def refine(self, loglevel: LogLevel = 1) -> None: ...
     def set_refine_criteria(
