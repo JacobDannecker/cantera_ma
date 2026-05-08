a53772087c12009f2a924262af2b6009e217b4fd
JacobDannecker <jacob.dannecker@stud.hs-mannheim.de>
2026-05-08 08:21:06 +0200

    --

--- Testscript/test_build.py
+++ Testscript/test_build.py
@@ -21,7 +21,7 @@ z_stoich = 0.111
 
 f.set_refine_criteria(ratio=3.0, slope=0.1, curve=0.2, prune=0.03)
 file_name = "Testscript/Data/test.h5"
-f.set_initial_guess(data=file_name, group="no_wall") 
+#f.set_initial_guess(data=file_name, group="no_wall") 
 # Set up wall 
 params = {
     'Z_wall': 0.8,
@@ -47,7 +47,7 @@ print(end - start)
 print(f.enthalpy_mass)
 print(f.transport_model)
 f.save(file_name, name="0800", overwrite=True)
-f2.restore(file_name, name="no_wall")
+f2.restore(file_name, name="0800")
 
 
 def chi_stoich(f, z_stoich):
